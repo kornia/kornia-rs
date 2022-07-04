44de7093e4f4ce671b1fcaadca67f424394c0c9c
edgar <edgar.riba@gmail.com>
2022-07-04 22:32:42 +0200

    add __version__ and fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -25,8 +25,19 @@ use crate::viz::show_image_from_tensor;
 
 use pyo3::prelude::*;
 
+pub fn get_version() -> String {
+    let version = env!("CARGO_PKG_VERSION").to_string();
+    // cargo uses "1.0-alpha1" etc. while python uses "1.0.0a1", this is not full compatibility,
+    // but it's good enough for now
+    // see https://docs.rs/semver/1.0.9/semver/struct.Version.html#method.parse for rust spec
+    // see https://peps.python.org/pep-0440/ for python spec
+    // it seems the dot after "alpha/beta" e.g. "-alpha.1" is not necessary, hence why this works
+    version.replace("-alpha", "a").replace("-beta", "b")
+}
+
 #[pymodule]
 pub fn kornia_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", get_version())?;
     m.add_function(wrap_pyfunction!(read_image_rs, m)?)?;
     m.add_function(wrap_pyfunction!(cvtensor_to_dlpack, m)?)?;
     m.add_class::<tensor::cv::Tensor>()?;
