a32405bb627ebe638ce91eed9dba577f41ddcf62
edgar <edgar.riba@gmail.com>
2024-03-25 23:05:50 +0100

    include python bindings

--- kornia-py/src/lib.rs
+++ kornia-py/src/lib.rs
@@ -1,3 +1,4 @@
+mod histogram;
 mod image;
 mod io;
 mod resize;
@@ -24,6 +25,7 @@ pub fn kornia_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(write_image_jpeg, m)?)?;
     m.add_function(wrap_pyfunction!(read_image_any, m)?)?;
     m.add_function(wrap_pyfunction!(resize::resize, m)?)?;
+    m.add_function(wrap_pyfunction!(histogram::compute_histogram, m)?)?;
     m.add_class::<PyImageSize>()?;
     m.add_class::<PyImageDecoder>()?;
     m.add_class::<PyImageEncoder>()?;
