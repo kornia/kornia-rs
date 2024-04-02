5bfb2ab790b0913f4b53689c28a0eda85dde9ef5
Thien Tran <gau.nernst@yahoo.com.sg>
2024-04-03 03:02:06 +0800

    Add Warp affine (#67)
    * add interpolate function for within-crate re-use
    
    * add warp_affine
    
    * add test. change affine matrix to simple tuple
    
    * add warp affine for python
    
    * fix test
    
    * fix out-of-bounds access for bilinear
    
    * add bounds check in warp affine
    
    * fix test
    
    * rename
    
    * fix function name
    
    * address comments
    
    * add get_rotation_matrix2d for easier testing
    
    * add bench
    
    * run lint
    
    * revert size interpret
    
    * use map_err
    
    * add docs and bench
    
    * remove unused import
    
    * fix dtype
    
    ---------
    
    Co-authored-by: edgar <edgar.riba@gmail.com>

--- kornia-py/src/lib.rs
+++ kornia-py/src/lib.rs
@@ -2,6 +2,7 @@ mod histogram;
 mod image;
 mod io;
 mod resize;
+mod warp;
 
 use crate::image::PyImageSize;
 use crate::io::functional::{read_image_any, read_image_jpeg, write_image_jpeg};
@@ -25,6 +26,7 @@ pub fn kornia_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(write_image_jpeg, m)?)?;
     m.add_function(wrap_pyfunction!(read_image_any, m)?)?;
     m.add_function(wrap_pyfunction!(resize::resize, m)?)?;
+    m.add_function(wrap_pyfunction!(warp::warp_affine, m)?)?;
     m.add_function(wrap_pyfunction!(histogram::compute_histogram, m)?)?;
     m.add_class::<PyImageSize>()?;
     m.add_class::<PyImageDecoder>()?;
---
+++ kornia-py/src/warp.rs
@@ -0,0 +1,47 @@
+use pyo3::prelude::*;
+
+use crate::image::{FromPyImage, PyImage, ToPyImage};
+use kornia_rs::image::Image;
+
+#[pyfunction]
+pub fn warp_affine(
+    image: PyImage,
+    m: (f32, f32, f32, f32, f32, f32),
+    new_size: (usize, usize),
+    interpolation: &str,
+) -> PyResult<PyImage> {
+    // have to add annotation Image<u8, 3>, otherwise the compiler will complain
+    // NOTE: do we support images with channels != 3?
+    let image: Image<u8, 3> = Image::from_pyimage(image)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
+
+    let new_size = kornia_rs::image::ImageSize {
+        height: new_size.0,
+        width: new_size.1,
+    };
+
+    let interpolation = match interpolation.to_lowercase().as_str() {
+        "nearest" => kornia_rs::resize::InterpolationMode::Nearest,
+        "bilinear" => kornia_rs::resize::InterpolationMode::Bilinear,
+        _ => {
+            return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+                "Invalid interpolation mode",
+            ))
+        }
+    };
+
+    // we need to cast to f32 for now since kornia-rs interpolation function only works with f32
+    let image = image
+        .cast::<f32>()
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
+
+    let image = kornia_rs::warp::warp_affine(&image, m, new_size, interpolation)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
+
+    // NOTE: for bicubic interpolation (not implemented yet), f32 may overshoot 255
+    let image = image
+        .cast::<u8>()
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
+
+    Ok(image.to_pyimage())
+}
