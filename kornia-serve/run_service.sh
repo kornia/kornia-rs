85e609527c76f7368d8a9501a73a0fbb466dfb28
edgar <edgar.riba@gmail.com>
2024-04-21 02:35:33 +0200

    add docker to run service

---
+++ kornia-serve/run_service.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get the mount volume from the first argument
+MOUNT_VOLUME=$1
+
+CONTAINER_ID=$(
+    docker run \
+        -d \
+        -e RUST_LOG=debug \
+        -v $MOUNT_VOLUME:$MOUNT_VOLUME \
+        -p 3000:3000 \
+        kornia-serve
+)
+
+stop_docker() {
+    docker stop $CONTAINER_ID
+}
+
+trap stop_docker SIGINT
+
+docker logs -f $CONTAINER_ID &
+LOG_PID=$!
+
+docker wait $CONTAINER_ID
+
+kill $LOG_PID
