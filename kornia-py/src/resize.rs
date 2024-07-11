b68c9ab79f6c8bd17777d3e4b72d893418c00672
edgar <edgar.riba@gmail.com>
2024-07-11 02:25:29 +0200

    fix python

--- kornia-py/src/resize.rs
+++ kornia-py/src/resize.rs
@@ -1,21 +1,24 @@
 use pyo3::prelude::*;
 
-use kornia_image::Image;
-use kornia_image::{FromPyImage, PyImage, ToPyImage};
+use crate::image::{FromPyImage, PyImage, ToPyImage};
+use kornia_rs::{
+    image::{Image, ImageSize},
+    imgproc::{interpolation::InterpolationMode, resize::resize_fast},
+};
 
 #[pyfunction]
 pub fn resize(image: PyImage, new_size: (usize, usize), interpolation: &str) -> PyResult<PyImage> {
     let image = Image::from_pyimage(image)
         .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
 
-    let new_size = kornia_image::ImageSize {
+    let new_size = ImageSize {
         height: new_size.0,
         width: new_size.1,
     };
 
     let interpolation = match interpolation.to_lowercase().as_str() {
-        "nearest" => kornia_rs::interpolation::InterpolationMode::Nearest,
-        "bilinear" => kornia_rs::interpolation::InterpolationMode::Bilinear,
+        "nearest" => InterpolationMode::Nearest,
+        "bilinear" => InterpolationMode::Bilinear,
         _ => {
             return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
                 "Invalid interpolation mode",
@@ -23,7 +26,7 @@ pub fn resize(image: PyImage, new_size: (usize, usize), interpolation: &str) -> 
         }
     };
 
-    let image = kornia_rs::resize::resize_fast(&image, new_size, interpolation)
+    let image = resize_fast(&image, new_size, interpolation)
         .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
 
     Ok(image.to_pyimage())
