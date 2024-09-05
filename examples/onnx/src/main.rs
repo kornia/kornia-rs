d9c461da76b1b2af8d8acdfd88f199576e3bfcae
edgar <edgar.riba@gmail.com>
2024-09-06 01:04:24 +0200

    update example

--- examples/onnx/src/main.rs
+++ examples/onnx/src/main.rs
@@ -3,7 +3,7 @@ use kornia::core::{CpuAllocator, Tensor};
 use kornia::imgproc::interpolation::InterpolationMode;
 use std::path::PathBuf;
 
-use kornia::image::Image;
+use kornia::image::{Image, ImageSize};
 use kornia::io::functional as F;
 
 #[derive(Parser)]
@@ -36,44 +36,208 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_intra_threads(4)?
         .commit_from_file(&args.onnx_model_path)?;
 
-    let new_size = [320, 320].into();
+    let new_size = [640, 640].into();
 
+    // resize the image
     let mut image_resized = Image::from_size_val(new_size, 0)?;
     kornia::imgproc::resize::resize_fast(&image, &mut image_resized, InterpolationMode::Bilinear)?;
 
-    let image = image_resized.clone().cast_and_scale::<f32>(1. / 255.)?;
+    // cast and scale the image to f32
+    let mut image_f32 = Image::from_size_val(image_resized.size(), 0.0)?;
+    kornia::image::ops::cast_and_scale(&image_resized, &mut image_f32, 1. / 255.)?;
 
-    let image_chw = image.permute_axes([2, 0, 1]);
+    // NOTE: this is the old way to do it and seems to work the rest nope
+    //let img_t = ndarray::Array3::from_shape_vec((640, 640, 3), image_f32.as_slice().to_vec())?;
+    //let img_t = img_t.view().permuted_axes([2, 0, 1]);
+    //let img_t = img_t.as_standard_layout().to_owned();
+    //let img_t = img_t.insert_axis(ndarray::Axis(0));
+    //let binding = img_t.to_owned();
+    //let shape = binding.shape();
+    //let ort_tensor: ort::Tensor<f32> = ort::Tensor::from_array((shape, img_t.into_raw_vec()))?;
+
+    // create a CHW view
+    let image_chw_view = image_f32.permute_axes([2, 0, 1]);
 
     // make it contiguous
-    let mut new_data = Vec::<f32>::with_capacity(3 * 320 * 320);
+    let mut image_chw_data = Vec::<f32>::with_capacity(3 * new_size.width * new_size.height);
 
     // TODO: implement Tensor::contiguous in kornia-core
-    for i in 0..image.rows() {
-        for j in 0..image.cols() {
-            for c in 0..image.num_channels() {
-                let value = image_chw.get_unchecked([i, j, c]);
-                new_data.push(*value);
+    // Convert the image to contiguous format from HWC to CHW
+    for c in 0..image_resized.num_channels() {
+        for i in 0..image_resized.rows() {
+            for j in 0..image_resized.cols() {
+                let value = image_chw_view.get_unchecked([i, j, c]);
+                image_chw_data.push(*value);
             }
         }
     }
-    let image_nchw = Tensor::from_shape_slice([1, 3, 320, 320], &new_data, CpuAllocator)?;
 
-    let ort_tensor = ort::Tensor::from_array((image_nchw.shape, image_nchw.into_vec()))?;
+    // make the rank 4 tensor
+    let image_nchw = Tensor::from_shape_vec(
+        [1, 3, new_size.height, new_size.width],
+        image_chw_data,
+        CpuAllocator,
+    )?;
+
+    // make the ort tensor
+    let ort_tensor: ort::Value<ort::TensorValueType<_>> =
+        ort::Tensor::from_array((image_nchw.shape, image_nchw.into_vec()))?;
+
+    //println!("ort_tensor: {:?}", ort_tensor.shape());
+
+    // get the inputs names
+    let inputs_names = model
+        .inputs
+        .iter()
+        .map(|x| x.name.to_string())
+        .collect::<Vec<_>>();
+
+    println!("inputs_name: {:?}", inputs_names);
+
+    // get the outputs names
+    let outputs_names = model
+        .outputs
+        .iter()
+        .map(|x| x.name.to_string())
+        .collect::<Vec<_>>();
+
+    println!("outputs_name: {:?}", outputs_names);
 
     // run the model
     let outputs = model.run(ort::inputs![
-        "images" => ort_tensor,
+        "input" => ort_tensor,
     ]?)?;
 
-    // get the output
-    let (loc_shape, loc) = outputs["loc"].try_extract_raw_tensor::<f32>()?;
-    let (conf_shape, conf) = outputs["conf"].try_extract_raw_tensor::<f32>()?;
-    let (iou_shape, iou) = outputs["iou"].try_extract_raw_tensor::<f32>()?;
+    // get the outputs
 
-    println!("loc: {:?}", loc);
-    println!("conf: {:?}", conf);
-    println!("iou: {:?}", iou);
+    let (out2_shape, out2_ort) = outputs["2794"].try_extract_raw_tensor::<f32>()?;
+    println!("out2_shape: {:?}", out2_shape);
+
+    let (out1_shape, out1_ort) = outputs["output"].try_extract_raw_tensor::<f32>()?;
+    println!("out1_shape: {:?}", out1_shape);
+
+    let shape = out1_shape.iter().map(|x| *x as usize).collect::<Vec<_>>();
+    let shape: [usize; 3] = shape.try_into().expect("Shape is too big");
+    let logits = Tensor::from_shape_slice(shape, out1_ort, CpuAllocator)?;
+
+    let shape = out2_shape.iter().map(|x| *x as usize).collect::<Vec<_>>();
+    let shape: [usize; 3] = shape.try_into().expect("Shape is too big");
+    let boxes = Tensor::from_shape_slice(shape, out2_ort, CpuAllocator)?;
+
+    let detections = post_process(&logits, &boxes, image_resized.size());
+
+    let detections = detections
+        .iter()
+        .filter(|d| d.score > 0.8)
+        .collect::<Vec<_>>();
+
+    println!("detections: {:?}", detections);
+
+    let mut boxes_mins = Vec::new();
+    let mut boxes_sizes = Vec::new();
+    for detection in detections {
+        boxes_mins.push((detection.x, detection.y));
+        boxes_sizes.push((detection.w, detection.h));
+    }
+
+    // create a Rerun recording stream
+    let rec = rerun::RecordingStreamBuilder::new("Kornia App").spawn()?;
+
+    rec.log(
+        "output",
+        &rerun::Image::from_elements(
+            image_resized.as_slice(),
+            image_resized.size().into(),
+            rerun::ColorModel::RGB,
+        ),
+    )?;
+
+    rec.log(
+        "boxes",
+        &rerun::Boxes2D::from_mins_and_sizes(boxes_mins, boxes_sizes),
+    )?;
 
     Ok(())
+}
+
+#[derive(Debug)]
+pub struct Detection {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+    pub score: f32,
+    pub label: u32,
+}
+
+// https://github.com/lyuwenyu/RT-DETR/blob/main/rtdetrv2_pytorch/src/zoo/rtdetr/rtdetr_postprocessor.py
+pub fn post_process(
+    logits: &Tensor<f32, 3>,
+    boxes: &Tensor<f32, 3>,
+    orig_target_size: ImageSize,
+) -> Vec<Detection> {
+    let mut detections = Vec::new();
+
+    let num_top_queries = 300;
+    let num_classes = 80;
+
+    let boxes_slice = boxes.as_slice();
+
+    let mut indexed_scores = logits
+        .as_slice()
+        .iter()
+        .enumerate()
+        .map(|(idx, x)| (idx, sigmoid(x)))
+        .collect::<Vec<_>>();
+
+    indexed_scores.sort_unstable_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+    let topk_scores = &indexed_scores[..num_top_queries];
+
+    for (index, score) in topk_scores {
+        println!("index: {:?}, score: {:?}", index, score);
+        let label = index % num_classes;
+        let box_idx = index / num_classes;
+
+        let box_i_offset = box_idx * 4;
+        let box_i = boxes_slice[box_i_offset..box_i_offset + 4]
+            .try_into()
+            .unwrap();
+
+        let box_out = box_cxcywh_to_xyxy(&box_i);
+
+        let (x, y, w, h) = (
+            box_out[0] * orig_target_size.width as f32,
+            box_out[1] * orig_target_size.height as f32,
+            box_out[2] * orig_target_size.width as f32,
+            box_out[3] * orig_target_size.height as f32,
+        );
+
+        let detection = Detection {
+            x,
+            y,
+            w,
+            h,
+            score: *score,
+            label: label as u32,
+        };
+
+        detections.push(detection);
+    }
+
+    detections
+}
+
+fn sigmoid(x: &f32) -> f32 {
+    1.0 / (1.0 + f32::exp(-x))
+}
+
+fn box_cxcywh_to_xyxy(boxes: &[f32; 4]) -> [f32; 4] {
+    let x_c = boxes[0];
+    let y_c = boxes[1];
+    let w = boxes[2];
+    let h = boxes[3];
+
+    let b = [x_c - 0.5 * w, y_c - 0.5 * h, x_c + 0.5 * w, y_c + 0.5 * h];
+
+    b
 }
