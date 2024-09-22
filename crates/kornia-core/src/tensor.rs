b312dc199d8c652c666baa87e6c913a684894c34
Jan Marais <maraisjandre9@gmail.com>
2024-09-22 22:38:13 +0200

    undo dev change of get_3d test

--- crates/kornia-core/src/tensor.rs
+++ crates/kornia-core/src/tensor.rs
@@ -1058,20 +1058,14 @@ mod tests {
 
     #[test]
     fn get_3d() -> Result<(), TensorError> {
-        let data: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-        let t = Tensor::<u8, 3>::from_shape_vec([2, 2, 3], data, CpuAllocator)?;
+        let data: Vec<u8> = vec![1, 2, 3, 4, 5, 6];
+        let t = Tensor::<u8, 3>::from_shape_vec([2, 1, 3], data, CpuAllocator)?;
         assert_eq!(t.get([0, 0, 0]), Some(&1));
         assert_eq!(t.get([0, 0, 1]), Some(&2));
         assert_eq!(t.get([0, 0, 2]), Some(&3));
-        assert_eq!(t.get([0, 1, 0]), Some(&4));
-        assert_eq!(t.get([0, 1, 1]), Some(&5));
-        assert_eq!(t.get([0, 1, 2]), Some(&6));
-        assert_eq!(t.get([1, 0, 0]), Some(&7));
-        assert_eq!(t.get([1, 0, 1]), Some(&8));
-        assert_eq!(t.get([1, 0, 2]), Some(&9));
-        assert_eq!(t.get([1, 1, 0]), Some(&10));
-        assert_eq!(t.get([1, 1, 1]), Some(&11));
-        assert_eq!(t.get([1, 1, 2]), Some(&12));
+        assert_eq!(t.get([1, 0, 0]), Some(&4));
+        assert_eq!(t.get([1, 0, 1]), Some(&5));
+        assert_eq!(t.get([1, 0, 2]), Some(&6));
         assert!(t.get([2, 0, 0]).is_none());
         assert!(t.get([0, 1, 0]).is_none());
         assert!(t.get([0, 0, 3]).is_none());
