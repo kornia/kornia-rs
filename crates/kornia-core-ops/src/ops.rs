cf202b48437a35ae15aa6697a4bc56ed4439e564
Jan Marais <maraisjandre9@gmail.com>
2024-10-02 19:54:58 +0200

    revert dynamic shapes

--- crates/kornia-core-ops/src/ops.rs
+++ crates/kornia-core-ops/src/ops.rs
@@ -1,75 +1,6 @@
-use kornia_core::{
-    storage::TensorStorage, tensor::get_strides_from_shape, CpuAllocator, SafeTensorType, Tensor,
-    TensorAllocator,
-};
+use kornia_core::{storage::TensorStorage, SafeTensorType, Tensor, TensorAllocator};
 
 use crate::error::TensorOpsError;
-
-trait HasShape {
-    type WithShape<New: Shape>: HasShape<Shape = New>;
-    type Shape: Shape;
-}
-
-trait Shape {}
-
-trait ReduceShapeTo<Dst> {
-    fn reduce(self, dim: usize) -> Dst;
-}
-
-trait SumTo: Sized + HasShape {
-    fn sum<Dst: Shape>(&self, dim: usize) -> Result<Self::WithShape<Dst>, TensorOpsError>
-    where
-        Self::Shape: ReduceShapeTo<Dst>;
-}
-
-impl<T: SafeTensorType + std::ops::Add<Output = T>, A: TensorAllocator>
-    ReduceShapeTo<Tensor<T, 2, A>> for Tensor<T, 3, A>
-{
-    fn reduce(self, dim: usize) -> Tensor<T, 2, A> {
-        let shape = match dim {
-            0 => [self.shape[1], self.shape[2]],
-            1 => [self.shape[0], self.shape[2]],
-            2 => [self.shape[0], self.shape[1]],
-            _ => panic!("dim out of bounds"),
-        };
-        let strides = get_strides_from_shape(shape);
-        Tensor {
-            storage: self.storage,
-            shape,
-            strides,
-        }
-    }
-}
-impl<T: SafeTensorType + std::ops::Add<Output = T>, A: TensorAllocator>
-    ReduceShapeTo<Tensor<T, 3, A>> for Tensor<T, 3, A>
-{
-    fn reduce(self, _dim: usize) -> Tensor<T, 3, A> {
-        self
-    }
-}
-
-impl<T: SafeTensorType + std::ops::Add<Output = T>, const N: usize, A: TensorAllocator> Shape
-    for Tensor<T, N, A>
-{
-}
-
-impl<S: Shape> HasShape for S {
-    type WithShape<New: Shape> = New;
-    type Shape = Self;
-}
-
-impl<T: SafeTensorType + std::ops::Add<Output = T>, const N: usize, A: TensorAllocator> SumTo
-    for Tensor<T, N, A>
-{
-    fn sum<Dst: Shape>(&self, dim: usize) -> Result<Self::WithShape<Dst>, TensorOpsError>
-    where
-        Self::Shape: ReduceShapeTo<Dst>,
-    {
-        let out = sum_elements(self, dim)?;
-        let out = out.reduce(dim);
-        Ok(out)
-    }
-}
 
 /// Compute the sum of the elements in the tensor along dimension `dim`
 ///
@@ -191,35 +122,6 @@ mod tests {
         assert_eq!(agg.as_slice(), [4; 6]);
         assert_eq!(sum_elements(&t_f32, 2).unwrap().as_slice(), [4.; 6]);
         assert_eq!(sum_elements(&t_i32, 2).unwrap().as_slice(), [4; 6]);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_sum_3d_dyn() -> Result<(), TensorError> {
-        let data: [u8; 24] = [1; 24];
-        let t = Tensor::<u8, 3>::from_shape_slice([2, 3, 4], &data, CpuAllocator)?;
-
-        let agg_full: Tensor<_, 3, _> = t.sum(0).unwrap();
-        let agg_reduced: Tensor<_, 2, _> = t.sum(0).unwrap();
-        assert_eq!(agg_full.shape, [1, 3, 4]);
-        assert_eq!(agg_full.as_slice(), [2; 12]);
-        assert_eq!(agg_reduced.shape, [3, 4]);
-        assert_eq!(agg_reduced.as_slice(), [2; 12]);
-
-        let agg_full: Tensor<_, 3, _> = t.sum(1).unwrap();
-        let agg_reduced: Tensor<_, 2, _> = t.sum(1).unwrap();
-        assert_eq!(agg_full.shape, [2, 1, 4]);
-        assert_eq!(agg_full.as_slice(), [3; 8]);
-        assert_eq!(agg_reduced.shape, [2, 4]);
-        assert_eq!(agg_reduced.as_slice(), [3; 8]);
-
-        let agg_full: Tensor<_, 3, _> = t.sum(2).unwrap();
-        let agg_reduced: Tensor<_, 2, _> = t.sum(2).unwrap();
-        assert_eq!(agg_full.shape, [2, 3, 1]);
-        assert_eq!(agg_full.as_slice(), [4; 6]);
-        assert_eq!(agg_reduced.shape, [2, 3]);
-        assert_eq!(agg_reduced.as_slice(), [4; 6]);
 
         Ok(())
     }
