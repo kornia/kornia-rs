98927455c9a97d0a072a2bcde4e3342aa1822a74
edgar <edgar.riba@gmail.com>
2024-12-21 23:24:25 +0100

    implement XYZRgbNormalsProperty in ply parser

--- crates/kornia-3d/src/io/ply/parser.rs
+++ crates/kornia-3d/src/io/ply/parser.rs
@@ -1,24 +1,12 @@
 use std::io::{BufRead, Read};
 use std::path::Path;
 
-use super::properties::{OpenSplatProperty, PlyProperty};
+use super::{properties::PlyType, PlyError, PlyPropertyTrait};
 use crate::pointcloud::PointCloud;
-
-#[derive(Debug, thiserror::Error)]
-pub enum PlyError {
-    #[error("Failed to read PLY file")]
-    Io(#[from] std::io::Error),
-
-    #[error("Failed to deserialize PLY file")]
-    Deserialize(#[from] bincode::Error),
-
-    #[error("Unsupported PLY property")]
-    UnsupportedProperty,
-}
 
 /// Read a PLY file in binary format.
 ///
-/// NOTE: This function only supports the OpenSplat PLY file format format for now.
+/// NOTE: This function only supports the OpenSplat and XYZRgbNormals PLY file format for now.
 /// REF: https://github.com/pierotofy/OpenSplat
 ///
 /// Args:
@@ -26,29 +14,23 @@ pub enum PlyError {
 ///
 /// Returns:
 ///     A `PointCloud` struct containing the points, colors, and normals.
-pub fn read_ply_binary(
-    path: impl AsRef<Path>,
-    property: PlyProperty,
-) -> Result<PointCloud, PlyError> {
-    if property != PlyProperty::OpenSplat {
-        return Err(PlyError::UnsupportedProperty);
-    }
-
+pub fn read_ply_binary(path: impl AsRef<Path>, property: PlyType) -> Result<PointCloud, PlyError> {
     // open the file
     let file = std::fs::File::open(path)?;
     let mut reader = std::io::BufReader::new(file);
 
     // read the header
-    // TODO support other formats headers
     let mut header = String::new();
     loop {
         let mut line = String::new();
         reader.read_line(&mut line)?;
         if line.starts_with("end_header") {
+            header.push_str(&line);
             break;
         }
         header.push_str(&line);
     }
+    println!("{:?}", header);
 
     // create a buffer for the points
     let mut buffer = vec![0u8; property.size_of()];
@@ -59,18 +41,10 @@ pub fn read_ply_binary(
     let mut normals = Vec::new();
 
     while reader.read_exact(&mut buffer).is_ok() {
-        match property {
-            PlyProperty::OpenSplat => {
-                let property: OpenSplatProperty = bincode::deserialize(&buffer)?;
-                points.push([property.x as f64, property.y as f64, property.z as f64]);
-                colors.push([
-                    (property.f_dc_0 * 255.0) as u8,
-                    (property.f_dc_1 * 255.0) as u8,
-                    (property.f_dc_2 * 255.0) as u8,
-                ]);
-                normals.push([property.nx as f64, property.ny as f64, property.nz as f64]);
-            }
-        }
+        let property_entry = property.deserialize(&buffer)?;
+        points.push(property_entry.to_point());
+        colors.push(property_entry.to_color());
+        normals.push(property_entry.to_normal());
     }
 
     Ok(PointCloud::new(points, Some(colors), Some(normals)))
--- crates/kornia-3d/src/io/ply/properties.rs
+++ crates/kornia-3d/src/io/ply/properties.rs
@@ -1,21 +1,58 @@
 use serde::Deserialize;
 
+use super::PlyError;
+
 /// Supported PLY properties.
 #[derive(Debug, PartialEq)]
-pub enum PlyProperty {
+pub enum PlyType {
     OpenSplat,
+    XYZRgbNormals,
 }
 
-impl PlyProperty {
-    /// Get the size of the property in bytes.
-    pub fn size_of(&self) -> usize {
-        match self {
-            PlyProperty::OpenSplat => std::mem::size_of::<OpenSplatProperty>(),
-        }
+/// Trait to represent the PLY property.
+pub trait PlyPropertyTrait {
+    /// Convert the property to a point.
+    fn to_point(&self) -> [f64; 3];
+
+    /// Convert the property to a color.
+    fn to_color(&self) -> [u8; 3];
+
+    /// Convert the property to a normal.
+    fn to_normal(&self) -> [f64; 3];
+}
+
+/// Header of the XYZRgbNormals PLY file format.
+///
+/// Contains points, colors, and normals.
+#[derive(Debug, Deserialize)]
+pub struct XYZRgbNormalsProperty {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+    pub nx: f32,
+    pub ny: f32,
+    pub nz: f32,
+}
+
+impl PlyPropertyTrait for XYZRgbNormalsProperty {
+    fn to_point(&self) -> [f64; 3] {
+        //println!("x: {}, y: {}, z: {}", self.x, self.y, self.z);
+        [self.x as f64, self.y as f64, self.z as f64]
+    }
+
+    fn to_color(&self) -> [u8; 3] {
+        [self.red, self.green, self.blue]
+    }
+
+    fn to_normal(&self) -> [f64; 3] {
+        [self.nx as f64, self.ny as f64, self.nz as f64]
     }
 }
 
-/// Header of the OpenSplat PLY file format.
+///// Header of the OpenSplat PLY file format.
 /// REF: https://github.com/pierotofy/OpenSplat
 #[derive(Debug, Deserialize)]
 pub struct OpenSplatProperty {
@@ -81,4 +118,74 @@ pub struct OpenSplatProperty {
     pub rot_1: f32,
     pub rot_2: f32,
     pub rot_3: f32,
+}
+
+impl PlyPropertyTrait for OpenSplatProperty {
+    fn to_point(&self) -> [f64; 3] {
+        [self.x as f64, self.y as f64, self.z as f64]
+    }
+
+    fn to_color(&self) -> [u8; 3] {
+        [
+            (self.f_dc_0 * 255.0) as u8,
+            (self.f_dc_1 * 255.0) as u8,
+            (self.f_dc_2 * 255.0) as u8,
+        ]
+    }
+
+    fn to_normal(&self) -> [f64; 3] {
+        [self.nx as f64, self.ny as f64, self.nz as f64]
+    }
+}
+
+/// Enum to represent the PLY property.
+pub enum PlyProperty {
+    OpenSplat(OpenSplatProperty),
+    XYZRgbNormals(XYZRgbNormalsProperty),
+}
+
+impl PlyType {
+    /// Deserialize the property from the buffer.
+    pub fn deserialize(&self, buffer: &[u8]) -> Result<PlyProperty, PlyError> {
+        match self {
+            PlyType::OpenSplat => {
+                let property: OpenSplatProperty = bincode::deserialize(buffer)?;
+                Ok(PlyProperty::OpenSplat(property))
+            }
+            PlyType::XYZRgbNormals => {
+                let property: XYZRgbNormalsProperty = bincode::deserialize(buffer)?;
+                Ok(PlyProperty::XYZRgbNormals(property))
+            }
+        }
+    }
+
+    pub fn size_of(&self) -> usize {
+        match self {
+            PlyType::OpenSplat => std::mem::size_of::<OpenSplatProperty>(),
+            PlyType::XYZRgbNormals => std::mem::size_of::<XYZRgbNormalsProperty>(),
+        }
+    }
+}
+
+impl PlyPropertyTrait for PlyProperty {
+    fn to_point(&self) -> [f64; 3] {
+        match self {
+            PlyProperty::OpenSplat(property) => property.to_point(),
+            PlyProperty::XYZRgbNormals(property) => property.to_point(),
+        }
+    }
+
+    fn to_color(&self) -> [u8; 3] {
+        match self {
+            PlyProperty::OpenSplat(property) => property.to_color(),
+            PlyProperty::XYZRgbNormals(property) => property.to_color(),
+        }
+    }
+
+    fn to_normal(&self) -> [f64; 3] {
+        match self {
+            PlyProperty::OpenSplat(property) => property.to_normal(),
+            PlyProperty::XYZRgbNormals(property) => property.to_normal(),
+        }
+    }
 }
