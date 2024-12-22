caa9d995575d41d4ae92963e3d2b5a505e0fff32
Edgar Riba <edgar.riba@gmail.com>
2024-12-23 00:39:40 +0100

    implement XYZRgbNormalsProperty in ply parser (#187)
    * implement XYZRgbNormalsProperty in ply parser
    
    * set add packed to remove padded bytes
    
    * fix readme
    
    * Update README.md
    
    * remove gstreamer in python
    
    * Apply suggestions from code review

--- crates/kornia-3d/src/io/ply/mod.rs
+++ crates/kornia-3d/src/io/ply/mod.rs
@@ -3,3 +3,15 @@ mod properties;
 
 pub use parser::*;
 pub use properties::*;
+
+#[derive(Debug, thiserror::Error)]
+pub enum PlyError {
+    #[error("Failed to read PLY file")]
+    Io(#[from] std::io::Error),
+
+    #[error("Failed to deserialize PLY file")]
+    Deserialize(#[from] bincode::Error),
+
+    #[error("Unsupported PLY property")]
+    UnsupportedProperty,
+}
