c914d53be5b58b749f0dee00c95bf1e4bfc50798
edgar <edgar.riba@gmail.com>
2024-12-27 15:56:26 +0100

    add benchmark

--- crates/kornia-imgproc/src/filter/mod.rs
+++ crates/kornia-imgproc/src/filter/mod.rs
@@ -1,8 +1,24 @@
-mod kernels;
-pub use kernels::*;
+//! Filter operations
+//!
+//! This module provides filter operations for image processing.
+//!
+//! # Example
+//!
+//! ```
+//! use kornia_rs::imgproc::filter::sobel;
+//!
+//! let image = Image::<f32, 3>::from_size_val([12, 12].into(), 0.0)?;
+//! let mut dst = Image::<f32, 3>::from_size_val(image.size(), 0.0)?;
+//! sobel(&image, &mut dst, 3)?;
+//! ```
 
+/// Filter kernels
+pub mod kernels;
+
+/// Filter operations
 mod ops;
 pub use ops::*;
 
+/// Separable filter operations
 mod separable_filter;
 pub use separable_filter::*;
