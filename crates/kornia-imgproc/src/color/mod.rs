885f6c57dda9316794a5a6192a2094b08028ac1f
edgar <edgar.riba@gmail.com>
2025-02-16 18:00:14 +0100

    color: implement gray from rgb as u8

--- crates/kornia-imgproc/src/color/mod.rs
+++ crates/kornia-imgproc/src/color/mod.rs
@@ -1,5 +1,5 @@
 mod gray;
 mod hsv;
 
-pub use gray::{bgr_from_rgb, gray_from_rgb, rgb_from_gray};
+pub use gray::{bgr_from_rgb, gray_from_rgb, gray_from_rgb_u8, rgb_from_gray};
 pub use hsv::hsv_from_rgb;
