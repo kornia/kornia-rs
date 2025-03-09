a512609c8b125de77cafb4b6480b09c1b554378c
Force1ess <forcelesszheng@foxmail.com>
2025-03-09 21:51:24 +0800

    add bench for dot_product1

--- crates/kornia-tensor-ops/benches/bench_ops.rs
+++ crates/kornia-tensor-ops/benches/bench_ops.rs
@@ -1,8 +1,30 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use kornia_tensor::{CpuAllocator, Tensor};
-use kornia_tensor_ops::ops::cosine_similarity;
+use kornia_tensor_ops::ops::{cosine_similarity, dot_product1};
 use rand::Rng;
+
+fn bench_dot_product1(c: &mut Criterion) {
+    let mut group = c.benchmark_group("dot_product1");
+    let mut rng = rand::rng();
+
+    let test_sizes = vec![8, 128, 1024, 16384];
+
+    for size in test_sizes {
+        let a: Vec<f32> = (0..size).map(|_| rng.random::<f32>()).collect();
+        let b: Vec<f32> = (0..size).map(|_| rng.random::<f32>()).collect();
+        let a_tensor =
+            Tensor::<f32, 1, CpuAllocator>::from_shape_slice([size], &a, CpuAllocator).unwrap();
+        let b_tensor =
+            Tensor::<f32, 1, CpuAllocator>::from_shape_slice([size], &b, CpuAllocator).unwrap();
+
+        group.bench_function(&format!("f32_size_{}", size), |bencher| {
+            bencher.iter(|| black_box(dot_product1(&a_tensor, &b_tensor).unwrap()))
+        });
+    }
+
+    group.finish();
+}
 
 fn bench_cosine_similarity(c: &mut Criterion) {
     let mut group = c.benchmark_group("cosine_similarity");
@@ -26,5 +48,5 @@ fn bench_cosine_similarity(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_cosine_similarity);
+criterion_group!(benches, bench_dot_product1, bench_cosine_similarity);
 criterion_main!(benches);
