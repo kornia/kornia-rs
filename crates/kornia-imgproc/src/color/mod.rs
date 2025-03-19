01095a3be990192de156a1285c65ea21515e1bc4
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2025-03-19 17:44:55 +0200

    Added rgb_from_yuv to color module. (#100)

--- crates/kornia-imgproc/src/color/mod.rs
+++ crates/kornia-imgproc/src/color/mod.rs
@@ -4,4 +4,4 @@ mod yuv;
 
 pub use gray::{bgr_from_rgb, gray_from_rgb, gray_from_rgb_u8, rgb_from_gray};
 pub use hsv::hsv_from_rgb;
-pub use yuv::yuv_from_rgb;
+pub use yuv::{rgb_from_yuv, yuv_from_rgb};
--- crates/kornia-imgproc/src/color/yuv.rs
+++ crates/kornia-imgproc/src/color/yuv.rs
@@ -74,11 +74,97 @@ pub fn yuv_from_rgb(src: &Image<f32, 3>, dst: &mut Image<f32, 3>) -> Result<(), 
     Ok(())
 }
 
+/// Convert a YUV image to an RGB image.
+///
+/// The input image is assumed to have 3 channels in the order Y, U, V. Where Y is in range[0, 1].
+/// U is in range [-0.436, +0.436] and V is in range [-0.615, +0.615].
+///
+/// # Arguments
+///
+/// * `src` - The input YUV image.
+/// * `dst` - The output RGB image.
+///
+/// # Returns
+///
+/// The RGB image with the following channels:
+///
+/// * R: The red channel in the range [0, 255].
+/// * G: The green channel in the range [0, 255].
+/// * B: The blue channel in the range [0, 255].
+///
+/// Precondition: the input image must have 3 channels.
+/// Precondition: the output image must have 3 channels.
+/// Precondition: the input and output images must have the same size.
+///
+/// # Example
+///
+/// ```
+/// use kornia_image::{Image, ImageSize};
+/// use kornia_imgproc::color::rgb_from_yuv;
+///
+/// let image = Image::<f32, 3>::new(
+///     ImageSize {
+///        width: 4,
+///        height: 5,
+///     },
+///     vec![0f32; 4 * 5 * 3],
+/// )
+/// .unwrap();
+///
+/// let mut yuv = Image::<f32, 3>::from_size_val(image.size(), 0.0).unwrap();
+///
+/// rgb_from_yuv(&image, &mut yuv).unwrap();
+///
+/// assert_eq!(yuv.num_channels(), 3);
+/// assert_eq!(yuv.size().width, 4);
+/// assert_eq!(yuv.size().height, 5);
+/// ```
+pub fn rgb_from_yuv(src: &Image<f32, 3>, dst: &mut Image<f32, 3>) -> Result<(), ImageError> {
+    if src.size() != dst.size() {
+        return Err(ImageError::InvalidImageSize(
+            src.cols(),
+            src.rows(),
+            dst.cols(),
+            dst.rows(),
+        ));
+    }
+
+    // compute the HSV values
+    parallel::par_iter_rows(src, dst, |src_pixel, dst_pixel| {
+        // Normalize the input to the range [0, 1]
+        let y = src_pixel[0] * 255.;
+        let u = src_pixel[1] * 255.;
+        let v = src_pixel[2] * 255.;
+
+        let r = y + 1.140 * v;
+        let g = y - 0.396 * u - 0.581 * v;
+        let b = y + 2.029 * u;
+
+        dst_pixel[0] = r;
+        dst_pixel[1] = g;
+        dst_pixel[2] = b;
+    });
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use kornia_image::{Image, ImageError, ImageSize};
     use num_traits::Pow;
-
+    const RGB_TEST_DATA: [f32; 18] = [
+        0.0, 128.0, 255.0, 255.0, 128.0, 0.0, 128.0, 255.0, 0.0, 255.0, 0.0, 128.0, 0.0, 128.0,
+        255.0, 255.0, 128.0, 0.0,
+    ];
+    const YUV_TEST_DATA: [f32; 18] = [
+        0.4087, 0.2909, -0.3585, 0.5937, -0.2921, 0.3565, 0.7371, -0.3628, -0.2063, 0.3562, 0.0719,
+        0.5648, 0.4087, 0.2909, -0.3585, 0.5937, -0.2921, 0.3565,
+    ];
+    const RGB_FROM_YUV_DATA: [f32; 18] = [
+        0.002548, 127.956985, 254.7287, 255.02805, 128.0725, 0.262405, 127.98908, 255.16042,
+        0.249588, 255.01837, -0.107399, 128.03171, 0.002548, 127.956985, 254.7287, 255.02805,
+        128.0725, 0.262405,
+    ];
     #[test]
     fn yuv_from_rgb() -> Result<(), ImageError> {
         let image = Image::<f32, 3>::new(
@@ -86,15 +172,9 @@ mod tests {
                 width: 2,
                 height: 3,
             },
-            vec![
-                0.0, 128.0, 255.0, 255.0, 128.0, 0.0, 128.0, 255.0, 0.0, 255.0, 0.0, 128.0, 0.0,
-                128.0, 255.0, 255.0, 128.0, 0.0,
-            ],
+            RGB_TEST_DATA.to_vec(),
         )?;
-        let expected = [
-            0.4087, 0.2909, -0.3585, 0.5937, -0.2921, 0.3565, 0.7371, -0.3628, -0.2063, 0.3562,
-            0.0719, 0.5648, 0.4087, 0.2909, -0.3585, 0.5937, -0.2921, 0.3565,
-        ];
+        let expected = YUV_TEST_DATA;
 
         let mut yuv = Image::<f32, 3>::from_size_val(image.size(), 0.0)?;
 
@@ -106,6 +186,52 @@ mod tests {
         for (a, b) in yuv.as_slice().iter().zip(expected.iter()) {
             println!("{:?} {:?}", a, b);
             assert!((a - b).pow(2) < 1e-6f32);
+        }
+        Ok(())
+    }
+    #[test]
+    fn rgb_from_yuv() -> Result<(), ImageError> {
+        let image = Image::<f32, 3>::new(
+            ImageSize {
+                width: 2,
+                height: 3,
+            },
+            YUV_TEST_DATA.to_vec(),
+        )?;
+        let expected = RGB_FROM_YUV_DATA;
+
+        let mut rgb = Image::<f32, 3>::from_size_val(image.size(), 0.0)?;
+
+        super::rgb_from_yuv(&image, &mut rgb)?;
+
+        assert_eq!(rgb.num_channels(), 3);
+        assert_eq!(rgb.size(), image.size());
+
+        for (a, b) in rgb.as_slice().iter().zip(expected.iter()) {
+            println!("{:?} {:?}", a, b);
+            assert!((a - b).pow(2) < 1e-6f32);
+        }
+        Ok(())
+    }
+    #[test]
+    fn yuv_inverse_relation_test() -> Result<(), ImageError> {
+        let image = Image::<f32, 3>::new(
+            ImageSize {
+                width: 2,
+                height: 3,
+            },
+            RGB_TEST_DATA.to_vec(),
+        )?;
+
+        let mut yuv = Image::<f32, 3>::from_size_val(image.size(), 0.0)?;
+        let mut rgb = Image::<f32, 3>::from_size_val(image.size(), 0.0)?;
+
+        super::yuv_from_rgb(&image, &mut yuv)?;
+        super::rgb_from_yuv(&yuv, &mut rgb)?;
+
+        for (a, b) in rgb.as_slice().iter().zip(image.as_slice().iter()) {
+            println!("{:?} {:?}", a, b);
+            assert!((a - b).pow(2) < 1e-1f32);
         }
         Ok(())
     }
