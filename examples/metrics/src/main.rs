849a7a84ef9bd96952ea1ccd697d24b64b963ad3
Kyle Kam <kamkyl196@gmail.com>
2025-03-19 05:40:56 -0700

    Replacing clap with argh.

--- examples/metrics/src/main.rs
+++ examples/metrics/src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use argh::FromArgs;
 use std::path::PathBuf;
 
 use kornia::io::functional as F;
@@ -8,14 +8,16 @@ use kornia::{
     imgproc,
 };
 
-#[derive(Parser)]
+#[derive(FromArgs)]
+/// Compute MSE and PSNR of an image and a dirtied image and log it to Rerun.
 struct Args {
-    #[arg(short, long)]
+    /// path to an input image
+    #[argh(option, short = 'i')]
     image_path: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
+    let args: Args = argh::from_env();
 
     // read the image
     let image: Image<u8, 3> = F::read_image_any_rgb8(args.image_path)?;
