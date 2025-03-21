fed4a0f3ce27ec48ca3dfdfc81bb383116ac7021
Andrew Shen <andrewshen31@gmail.com>
2025-03-21 12:23:49 -0700

    using glam and initial implementation of `SO3`

--- crates/kornia-liealg/src/so3.rs
+++ crates/kornia-liealg/src/so3.rs
@@ -1,61 +1,111 @@
 use std::fmt::Debug;
 use std::ops::Mul;
+use glam::{Affine3A, Mat3, Mat3A, Mat4, Quat, Vec3};
+use rand::Rng;
+
 
 pub struct SO3 {
-    pub quaternion: [f32; 4],
+    pub quaternion: Quat,
 }
 
 impl SO3 {
     pub fn identity() -> Self {
-        Self { quaternion: [1., 0., 0., 0.] }
+        Self { quaternion: Quat::IDENTITY }
     }
 
-    pub fn from_quaternion(q: [f32; 4]) -> Self {
-        Self { quaternion: q }
-    }
-
-    pub fn from_matrix(m: [[f32; 4]; 4]) -> Self {
-        todo!()
+    pub fn from_matrix(mat: &Mat4) -> Self {
+        Self { quaternion: Quat::from_mat4(mat)}
     }
 
     pub fn from_random() -> Self {
-        todo!()
+        let mut rng = rand::rng();
+
+        let r1: f32 = rng.random();
+        let r2: f32 = rng.random();
+        let r3: f32 = rng.random();
+
+        let w = (1.0 - r1).sqrt() * (2.0 * std::f32::consts::PI * r2).sin();
+        let x = (1.0 - r1).sqrt() * (2.0 * std::f32::consts::PI * r2).cos();
+        let y = r1.sqrt() * (2.0 * std::f32::consts::PI * r3).sin();
+        let z = r1.sqrt() * (2.0 * std::f32::consts::PI * r3).cos();
+
+        Self {
+            quaternion: Quat::from_xyzw(x, y, z, w)
+        }
     }
 
-    pub fn adjoint(&self) -> [[f32; 3]; 3] {
-        todo!()
+    pub fn to_matrix(&self) -> Mat3A {
+        Affine3A::from_quat(self.quaternion).matrix3
     }
 
-    pub fn inverse(&self) -> [[f32; 3]; 3] {
-        todo!()
+    pub fn adjoint(&self) -> Mat3A {
+        self.to_matrix()
     }
 
-    pub fn exp(v: [f32; 3]) -> Self {
-        todo!()
+    pub fn inverse(&self) -> Self {
+        Self { quaternion: self.quaternion.inverse() }
     }
 
-    pub fn log(&self) -> [f32; 3] {
-        todo!()
+    /// Lie algebra -> Lie group
+    pub fn exp(v: Vec3) -> Self {
+        let theta = v.dot(v).sqrt();
+        let theta_half = theta / 2.0;
+
+        let (w,b) = if theta != 0.0 {
+            (theta_half.cos(), theta_half.sin()/theta)
+        } else {
+            (1.0,0.0)
+        };
+        let xyz = b*v;
+
+        Self {
+            quaternion: Quat::from_xyzw(xyz.x, xyz.y, xyz.z, w),
+        }
     }
 
-    pub fn to_matrix(&self) -> [f32; 3] {
-        todo!()
+    /// Lie group -> Lie algebra
+    pub fn log(&self) -> Vec3 {
+        let real = self.quaternion.w;
+        let vec = Vec3::new(self.quaternion.x, self.quaternion.y, self.quaternion.z);
+
+        let theta = vec.dot(vec).sqrt();
+        let omega = if theta != 0.0 {
+            vec*2.8*real.acos() / theta
+        } else {
+            vec*2.0 /real
+        };
+
+        omega
     }
 
-    pub fn hat(v: [f32; 3]) -> Self {
-        todo!()
+    /// Vector space -> Lie algebra
+    pub fn hat(v: Vec3) -> Mat3 {
+        Mat3::from_cols_array(&[
+             0.0, -v.z, v.y,
+             v.z,  0.0,-v.x,
+            -v.y,  v.x, 0.0,
+        ])
     }
 
-    pub fn vee(omega: [[f32; 3]; 3]) -> Self {
-        todo!()
+    /// Lie algebra -> vector space
+    pub fn vee(omega: Mat3) -> Vec3 {
+        Vec3::from_array([omega[7], omega[2], omega[3]])
     }
 
-    pub fn left_jacobian(m: [[f32; 4]; 4]) -> Self {
-        todo!()
+    pub fn left_jacobian(v: Vec3) -> Mat3 {
+        let skew = Self::hat(v);
+        let theta = v.dot(v).sqrt();
+        let ident = Mat3::IDENTITY;
+
+        ident+((1.0-theta.cos())/theta.powi(2))*skew + ((theta-theta.sin()) / theta.powi(3)) * (skew*skew)
     }
 
-    pub fn right_jacobian(m: [[f32; 4]; 4]) -> Self {
-        todo!()
+    pub fn right_jacobian(v: Vec3) -> Mat3 {
+        let skew = Self::hat(v);
+        let theta = v.dot(v).sqrt();
+        let ident = Mat3::IDENTITY;
+
+        ident-((1.0-theta.cos())/theta.powi(2))*skew + ((theta-theta.sin()) / theta.powi(3)) * (skew*skew)
     }
 }
 
