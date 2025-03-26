40f0ec5536c4acbc797780a86c25e7d52f7a4922
Andrew Shen <39780709+andrew-shc@users.noreply.github.com>
2025-03-26 15:10:29 -0700

    implemented `harris_response` (#267)
    * initial commit
    
    * Delete .idea directory
    
    * attempted to pass the python example test
    
    * camelcase enums
    
    * switched to manual summation & benchmarking
    
    * cleaned code & more benchmarking
    
    * cleaned code & more benchmarking
    
    * attempted autovectorization + using filter::separable_filter
    
    * performance optimization update
    
    * performance optimization
    
    * kept the version of manual summation w/ slicing & w/o bounds checking + updated test + cleaned up code
    
    * removed `wide` crate
    
    * added builder pattern
    
    * additional comments needed to remove
    
    * moved vector data to `HarrisResponse` + `get_unchecked` on slicings
    
    * initialize gradient data at initialization with fixed length
    
    * more tests, fixed bugs, cleaned code
    
    * Available recipes:
        check                             # Check if the code is linting and formatted
        check-environment                 # Check if the required binaries for the project are installed
        clean                             # Clean up caches and build artifacts
        clippy                            # Run clippy with all features
        clippy-default                    # Run clippy with default features
        format                            # Run autoformatting and linting
        py-build py_version='3.9'         # Create virtual environment, and build kornia-py
        py-build-release py_version='3.9' # Create virtual environment, and build kornia-py for release
        py-install py_version='3.9'       # Create virtual environment, and install dev requirements
        py-test                           # Test the kornia-py code with pytest
        test name=''                      # Test the code or a specific test
        test-all                          # Test the code with all features routine
    
    * simplify unsafe and add comments
    
    * removed TODOs
    
    ---------
    
    Co-authored-by: edgar <edgar.riba@gmail.com>

--- crates/kornia-imgproc/benches/bench_features.rs
+++ crates/kornia-imgproc/benches/bench_features.rs
@@ -1,9 +1,43 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use kornia_image::Image;
-use kornia_imgproc::features::dog_response;
+use kornia_imgproc::features::{dog_response, HarrisResponse};
+use rand::Rng;
+
+fn bench_harris_response(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Features");
+    let mut rng = rand::thread_rng();
+
+    for (width, height) in [(1920, 1080)].iter() {
+        group.throughput(criterion::Throughput::Elements((*width * *height) as u64));
+
+        let parameter_string = format!("{}x{}", width, height);
+
+        // input image
+        let image_data: Vec<f32> = (0..(*width * *height))
+            .map(|_| rng.gen_range(0.0..1.0))
+            .collect();
+        let image_size = [*width, *height].into();
+
+        let image_f32: Image<f32, 1> = Image::new(image_size, image_data).unwrap();
+
+        // output image
+        let response_f32: Image<f32, 1> = Image::from_size_val(image_size, 0.0).unwrap();
+        let mut harris_response = HarrisResponse::new(image_size);
+
+        group.bench_with_input(
+            BenchmarkId::new("harris", &parameter_string),
+            &(&image_f32, &response_f32),
+            |b, i| {
+                let (src, mut dst) = (i.0, i.1.clone());
+                b.iter(|| black_box(harris_response.compute(src, &mut dst)))
+            },
+        );
+    }
+    group.finish();
+}
 
 fn bench_dog_response(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Dog Response");
+    let mut group = c.benchmark_group("Features");
     group.sample_size(30);
     let test_sizes = [(32, 32), (512, 512), (8192, 8192)];
 
@@ -34,7 +68,11 @@ fn bench_dog_response(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_dog_response);
+criterion_group!(
+    name = benches;
+    config = Criterion::default().warm_up_time(std::time::Duration::new(10, 0));
+    targets = bench_harris_response, bench_dog_response
+);
 criterion_main!(benches);
 
 #[cfg(test)]
