6e679e1820b971b780296cc54def1b520723679d
aayushhzz <aayushuzi001@gmail.com>
2025-04-05 14:14:29 +0530

    moved gftt response function to responses.rs

--- crates/kornia-imgproc/src/features/responses.rs
+++ crates/kornia-imgproc/src/features/responses.rs
@@ -1,6 +1,10 @@
 use crate::filter::gaussian_blur;
 use kornia_image::{Image, ImageError, ImageSize};
+use kornia_tensor_ops::TensorOps;
 use rayon::prelude::*;
+
+use crate::filter::gaussian_blur;
+use crate::filter::spatial_gradient_float;
 
 /// Method to calculate gradient for feature response
 #[derive(Default)]
@@ -23,6 +27,77 @@ fn _get_kernel_size(sigma: f32) -> usize {
     }
 
     ksize
+}
+///Compute the Shi-Tomasi cornerness function.
+///
+/// The Shi-Tomasi cornerness function is computed as the minimum eigenvalue of the gradient matrix.
+///
+/// Args:
+///    src: The source image.
+///   dst: The destination image.
+pub fn gftt_response(src: &Image<f32, 1>, dst: &mut Image<f32, 1>) -> Result<(), ImageError> {
+    if src.size() != dst.size() {
+        return Err(ImageError::InvalidImageSize(
+            src.cols(),
+            src.rows(),
+            dst.cols(),
+            dst.rows(),
+        ));
+    }
+    let mut dx = Image::from_size_val(src.size(), 0.0)?;
+    let mut dy = Image::from_size_val(src.size(), 0.0)?;
+    let _ = spatial_gradient_float(src, &mut dx, &mut dy);
+    let dx2: Image<f32, 1> = Image(dx.mul(&dx).unwrap());
+    let dy2: Image<f32, 1> = Image(dy.mul(&dy).unwrap());
+    let dxy: Image<f32, 1> = Image(dx.mul(&dy).unwrap());
+
+    let mut dx2_g = Image::from_size_val(src.size(), 0.0)?;
+    let mut dy2_g = Image::from_size_val(src.size(), 0.0)?;
+    let mut dxy_g = Image::from_size_val(src.size(), 0.0)?;
+    let _ = gaussian_blur(&dx2, &mut dx2_g, (7usize, 7usize), (1.0, 1.0));
+    let _ = gaussian_blur(&dy2, &mut dy2_g, (7usize, 7usize), (1.0, 1.0));
+    let _ = gaussian_blur(&dxy, &mut dxy_g, (7usize, 7usize), (1.0, 1.0));
+
+    let det_m = dx2_g
+        .mul(&dy2_g)
+        .unwrap()
+        .sub(&dxy_g.mul(&dxy_g).unwrap())
+        .unwrap();
+    let trace_m = dx2_g.add(&dy2_g).unwrap();
+
+    let e1 = trace_m
+        .add(
+            &(trace_m
+                .mul(&trace_m)
+                .unwrap()
+                .sub(&det_m.mul_scalar(4.0))
+                .unwrap()
+                .abs())
+            .powf(0.5),
+        )
+        .unwrap()
+        .mul_scalar(0.5);
+    let e2 = trace_m
+        .sub(
+            &(trace_m
+                .mul(&trace_m)
+                .unwrap()
+                .sub(&det_m.mul_scalar(4.0))
+                .unwrap()
+                .abs())
+            .powf(0.5),
+        )
+        .unwrap()
+        .mul_scalar(0.5);
+
+    let score = e1.min(&e2).unwrap();
+    dst.as_slice_mut()
+        .iter_mut()
+        .zip(score.as_slice().iter())
+        .for_each(|(dst_pixel, score_pixel)| {
+            *dst_pixel = *score_pixel;
+        });
+    Ok(())
 }
 
 /// Compute the Hessian response of an image.
@@ -325,7 +400,44 @@ pub fn dog_response(
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn test_gftt_response() -> Result<(), ImageError> {
+        #[rustfmt::skip]
+        let src = Image::from_size_slice(
+            [9, 9].into(),
+            &[
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+                ],
+        )?;
 
+        let mut dst = Image::from_size_val([9, 9].into(), 0.0)?;
+        gftt_response(&src, &mut dst)?;
+
+        #[rustfmt::skip]
+        let expected_center_value = 0.1274;
+        assert!(
+            (dst.as_slice()[4 * 9 + 4] - expected_center_value).abs() < 1e-4,
+            "Center value should be close to expected value"
+        );
+        let max = dst
+            .as_slice()
+            .iter()
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap();
+        assert!(
+            (*max - expected_center_value).abs() < 1e-4,
+            "Max value should be close to centre value"
+        );
+        Ok(())
+    }
     #[test]
     fn test_hessian_response() -> Result<(), ImageError> {
         #[rustfmt::skip]
