cb33778d36f079f6e45533050a1c6ca429728a60
edgar <edgar.riba@gmail.com>
2025-05-04 13:41:41 +0200

    python: rework jpeg and jpegturbo decoders

--- kornia-py/src/io/jpegturbo.rs
+++ kornia-py/src/io/jpegturbo.rs
@@ -1,6 +1,8 @@
 use crate::image::{FromPyImage, PyImage, PyImageSize, ToPyImage};
 use kornia_image::Image;
-use kornia_io::jpegturbo::{JpegTurboDecoder, JpegTurboEncoder};
+use kornia_io::jpegturbo::{
+    read_image_jpegturbo_rgb8, write_image_jpegturbo_rgb8, JpegTurboDecoder, JpegTurboEncoder,
+};
 use pyo3::prelude::*;
 
 #[pyclass(name = "ImageDecoder", frozen)]
@@ -68,4 +70,60 @@ impl PyImageEncoder {
             .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
         Ok(())
     }
+}
+
+#[pyfunction]
+pub fn read_image_jpegturbo(file_path: &str) -> PyResult<PyImage> {
+    let image = read_image_jpegturbo_rgb8(file_path)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyFileExistsError, _>(format!("{}", e)))?;
+    Ok(image.to_pyimage())
+}
+
+#[pyfunction]
+pub fn write_image_jpegturbo(file_path: &str, image: PyImage, quality: u8) -> PyResult<()> {
+    let image = Image::from_pyimage(image)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
+    write_image_jpegturbo_rgb8(file_path, &image, quality)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?;
+    Ok(())
+}
+
+#[pyfunction]
+/// Decodes the JPEG Image from raw bytes.
+///
+/// The following modes are supported:
+/// 1. "rgb" -> 8-bit RGB
+/// 2. "mono" -> 8-bit Monochrome
+///
+/// ```py
+/// import kornia_rs as K
+///
+/// img = K.decode_image_jpegturbo(bytes(img_data), "rgb")
+/// ```
+pub fn decode_image_jpegturbo(jpeg_data: &[u8], mode: &str) -> PyResult<PyImage> {
+    let image = match mode {
+        "rgb" => JpegTurboDecoder::new()
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?
+            .decode_rgb8(jpeg_data)
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?
+            .to_pyimage(),
+        "mono" => JpegTurboDecoder::new()
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?
+            .decode_gray8(jpeg_data)
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))?
+            .to_pyimage(),
+        _ => {
+            return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+                String::from(
+                    r#"\
+        The following are the supported values of mode:
+        1) "rgb" -> 8-bit RGB
+        2) "mono" -> 8-bit Monochrome
+        "#,
+                ),
+            ))
+        }
+    };
+
+    Ok(image)
 }
