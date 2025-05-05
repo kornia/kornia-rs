8cf584c352a980c86a7e9277f38dab9f0f5c878e
Edgar Riba <edgar.riba@gmail.com>
2025-05-05 10:01:17 +0200

    python: rework jpeg and jpegturbo decoders (#365)
    * python: rework jpeg and jpegturbo decoders
    
    * disable kornia py
    
    * fix docs

--- kornia-py/src/io/jpeg.rs
+++ kornia-py/src/io/jpeg.rs
@@ -1,7 +1,66 @@
-use crate::image::{PyImage, ToPyImage};
+use crate::image::{FromPyImage, PyImage, ToPyImage};
 use kornia_image::{Image, ImageSize};
 use kornia_io::jpeg as J;
 use pyo3::prelude::*;
+
+#[pyfunction]
+pub fn read_image_jpeg(file_path: &str, mode: &str) -> PyResult<PyImage> {
+    let result = match mode {
+        "rgb" => {
+            let img = J::read_image_jpeg_rgb8(file_path)
+                .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
+            img.to_pyimage()
+        }
+        "mono" => {
+            let img = J::read_image_jpeg_mono8(file_path)
+                .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
+            img.to_pyimage()
+        }
+        _ => {
+            return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+                String::from(
+                    r#"\
+        The following are the supported values of mode:
+        1) "rgb" -> 8-bit RGB
+        2) "mono" -> 8-bit Monochrome
+        "#,
+                ),
+            ))
+        }
+    };
+
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn write_image_jpeg(file_path: &str, image: PyImage, mode: &str, quality: u8) -> PyResult<()> {
+    match mode {
+        "rgb" => {
+            let image = Image::<u8, 3>::from_pyimage(image)
+                .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
+            J::write_image_jpeg_rgb8(file_path, &image, quality)
+                .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
+        }
+        "mono" => {
+            let image = Image::<u8, 1>::from_pyimage(image)
+                .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
+            J::write_image_jpeg_gray8(file_path, &image, quality)
+                .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
+        }
+        _ => {
+            return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+                String::from(
+                    r#"\
+        The following are the supported values of mode:
+        1) "rgb" -> 8-bit RGB
+        2) "mono" -> 8-bit Monochrome
+        "#,
+                ),
+            ))
+        }
+    }
+    Ok(())
+}
 
 #[pyfunction]
 /// Decodes the JPEG Image from raw bytes.
@@ -13,14 +72,10 @@ use pyo3::prelude::*;
 /// ```py
 /// import kornia_rs as K
 ///
-/// img = K.decode_image_raw_jpeg(bytes(img_data), (32, 32), "rgb")
+/// img = K.decode_image_jpeg(bytes(img_data), (32, 32), "rgb")
 /// ```
 ///
-pub fn decode_image_raw_jpeg(
-    src: &[u8],
-    image_shape: (usize, usize),
-    mode: &str,
-) -> PyResult<PyImage> {
+pub fn decode_image_jpeg(src: &[u8], image_shape: (usize, usize), mode: &str) -> PyResult<PyImage> {
     let image_shape = ImageSize {
         width: image_shape.1,
         height: image_shape.0,
--- kornia-py/tests/test_io.py
+++ kornia-py/tests/test_io.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import tempfile
 from pathlib import Path
 from typing import Callable
@@ -14,10 +15,11 @@ DATA_DIR = Path(__file__).parents[2] / "tests" / "data"
 def _test_write_read_impl(
     dtype: np.dtype,
     channels: int,
-    mode: str,
     file_name: str,
     fcn_write: Callable,
     fcn_read: Callable,
+    mode: str | None = None,
+    quality: int | None = None,
 ) -> None:
     img = np.array(
         [
@@ -33,10 +35,14 @@ def _test_write_read_impl(
     # write the image to a file
     with tempfile.TemporaryDirectory() as tmpdir:
         img_path = Path(tmpdir) / file_name
-        fcn_write(str(img_path), img, mode)
+        read_kwargs = {} if mode is None else {"mode": mode}
+        write_kwargs = read_kwargs.copy()
+        if quality is not None:
+            write_kwargs["quality"] = quality
+        fcn_write(str(img_path), img, **write_kwargs)
 
         # read the image back
-        img_read = fcn_read(str(img_path), mode)
+        img_read = fcn_read(str(img_path), **read_kwargs)
 
     # check the image properties
     assert img_read.shape == (4, 5, channels)
@@ -46,10 +52,11 @@ def _test_write_read_impl(
 def test_read_image_jpeg():
     # load an image with libjpeg-turbo
     img_path: Path = DATA_DIR / "dog.jpeg"
-    img: np.ndarray = K.read_image_jpeg(str(img_path.absolute()))
+    img: np.ndarray = K.read_image_jpeg(str(img_path.absolute()), "rgb")
 
     # check the image properties
     assert img.shape == (195, 258, 3)
+    assert img.dtype == np.uint8
 
     img_t = torch.from_numpy(img)
     assert img_t.shape == (195, 258, 3)
@@ -60,23 +67,25 @@ def test_decode_image_jpeg():
     img_path: Path = DATA_DIR / "dog.jpeg"
     with open(img_path, "rb") as f:
         img_data = f.read()
-    img: np.ndarray = K.decode_image_jpeg(bytes(img_data), "rgb")
+    img: np.ndarray = K.decode_image_jpeg(bytes(img_data), (195, 258), "rgb")
 
     # check the image properties
     assert img.shape == (195, 258, 3)
+    assert img.dtype == np.uint8
 
     img_t = torch.from_numpy(img)
     assert img_t.shape == (195, 258, 3)
 
 
-def test_decode_image_raw_jpeg():
+def test_decode_image_jpegturbo():
     img_path: Path = DATA_DIR / "dog.jpeg"
     with open(img_path, "rb") as f:
         img_data = f.read()
-    img: np.ndarray = K.decode_image_raw_jpeg(bytes(img_data), (195, 258), "rgb")
+    img: np.ndarray = K.decode_image_jpegturbo(bytes(img_data), "rgb")
 
     # check the image properties
     assert img.shape == (195, 258, 3)
+    assert img.dtype == np.uint8
 
     img_t = torch.from_numpy(img)
     assert img_t.shape == (195, 258, 3)
@@ -111,7 +120,6 @@ def test_decode_image_png_u16():
     assert img_t.shape == (32, 32, 3)
 
 
-# TODO: load other types of images
 def test_read_image_any():
     # load an image with image-rs
     img_path: Path = DATA_DIR / "dog.jpeg"
@@ -119,7 +127,7 @@ def test_read_image_any():
 
     # check the image properties
     assert img.shape == (195, 258, 3)
-
+    assert img.dtype == np.uint8
     img_t = torch.from_numpy(img)
     assert img_t.shape == (195, 258, 3)
 
@@ -127,7 +135,7 @@ def test_read_image_any():
 def test_decompress():
     # load an image with libjpeg-turbo
     img_path: Path = DATA_DIR / "dog.jpeg"
-    img: np.ndarray = K.read_image_jpeg(str(img_path))
+    img: np.ndarray = K.read_image_jpeg(str(img_path), "rgb")
 
     image_encoder = K.ImageEncoder()
     image_encoded: list[int] = image_encoder.encode(img)
@@ -169,29 +177,39 @@ def test_compress_decompress():
     assert (decoded_img - img).sum() == 3
 
 
-def test_write_read_jpeg():
-    img = np.array(
-        [
-            [0, 0, 0, 0, 0],
-            [0, 127, 255, 127, 0],
-            [0, 127, 255, 127, 0],
-            [0, 127, 255, 127, 0],
-        ],
+def test_write_read_jpeg_rgb8():
+    _test_write_read_impl(
         dtype=np.uint8,
+        channels=3,
+        file_name="test_write_read_jpeg.jpg",
+        fcn_write=K.write_image_jpeg,
+        fcn_read=K.read_image_jpeg,
+        mode="rgb",
+        quality=100,
     )
-    img = np.repeat(img[..., None], 3, axis=-1)
 
-    # write the image to a file
-    with tempfile.TemporaryDirectory() as tmpdir:
-        img_path = Path(tmpdir) / "test_write_read_jpeg.jpg"
-        K.write_image_jpeg(str(img_path), img)
 
-        # read the image back
-        img_read = K.read_image_jpeg(str(img_path))
+def test_write_read_jpeg_mono8():
+    _test_write_read_impl(
+        dtype=np.uint8,
+        channels=1,
+        file_name="test_write_read_jpeg.jpg",
+        fcn_write=K.write_image_jpeg,
+        fcn_read=K.read_image_jpeg,
+        mode="mono",
+        quality=100,
+    )
 
-    # check the image properties
-    assert img_read.shape == (4, 5, 3)
-    np.allclose(img, img_read)
+
+def test_write_read_jpegturbo():
+    _test_write_read_impl(
+        dtype=np.uint8,
+        channels=3,
+        file_name="test_write_read_jpegturbo.jpg",
+        fcn_write=K.write_image_jpegturbo,
+        fcn_read=K.read_image_jpegturbo,
+        quality=100,
+    )
 
 
 def test_write_read_tiff_rgb8():
