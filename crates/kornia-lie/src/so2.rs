5dbd011f845ec9a07d6177f57b27455b7447c663
Andrew Shen <39780709+andrew-shc@users.noreply.github.com>
2025-05-26 08:05:09 -0700

    Create `kornia-lie` crate on top of glam-rs (#303)
    * initial implementation of so3
    
    * using glam and initial implementation of `SO3`
    
    * renamed to
    
    * empty tests
    
    * added tests for SO3
    
    * init impl of se3, so2, se2
    
    * implemented SO2
    
    * various fixes
    
    * added tests for SO2
    
    * basic impl of SO3 & SO2
    
    * implemented se2 & se2
    
    * added tests for SE3 and SE2
    
    * added raw 6x6 matrix for the adjoint of SE3
    
    * improved code
    
    * fixed test failures
    
    * cargo fmt
    
    * Update Cargo.toml
    
    ---------
    
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>

---
+++ crates/kornia-lie/src/so2.rs
@@ -0,0 +1,187 @@
+use std::ops::Mul;
+
+use glam::{Mat2, Mat3A, Vec2};
+use rand::Rng;
+
+#[derive(Debug, Clone, Copy)]
+pub struct SO2 {
+    /// representing complex number [real, imaginary]
+    pub z: Vec2,
+}
+
+impl SO2 {
+    pub const IDENTITY: Self = Self {
+        z: Vec2 { x: 1.0, y: 0.0 },
+    };
+
+    pub fn new(z: Vec2) -> Self {
+        Self { z }
+    }
+
+    pub fn from_matrix(mat: Mat2) -> Self {
+        Self {
+            z: Vec2 {
+                x: mat.x_axis.x,
+                y: mat.y_axis.x,
+            },
+        }
+    }
+
+    pub fn from_matrix3a(mat: Mat3A) -> Self {
+        Self {
+            z: Vec2 {
+                x: mat.x_axis.x,
+                y: mat.y_axis.x,
+            },
+        }
+    }
+
+    pub fn from_random() -> Self {
+        let mut rng = rand::rng();
+
+        let r1: f32 = rng.random();
+        let r2: f32 = rng.random();
+
+        Self {
+            z: Vec2 { x: r1, y: r2 },
+        }
+    }
+
+    pub fn matrix(&self) -> Mat2 {
+        Mat2::from_cols_array(&[self.z.x, -self.z.y, self.z.y, self.z.x])
+    }
+
+    /// inverting the complex number z (represented as a 2D vector)
+    pub fn inverse(&self) -> Self {
+        let c: f32 = self.z.dot(self.z);
+        Self {
+            z: Vec2::new(self.z.x / c, -self.z.y / c),
+        }
+    }
+
+    pub fn adjoint(&self) -> Mat2 {
+        Self::IDENTITY.matrix()
+    }
+
+    pub fn exp(theta: f32) -> Self {
+        Self {
+            z: Vec2 {
+                x: theta.cos(),
+                y: theta.sin(),
+            },
+        }
+    }
+
+    pub fn log(&self) -> f32 {
+        self.z.y.atan2(self.z.x)
+    }
+
+    pub fn hat(theta: f32) -> Mat2 {
+        Mat2::from_cols_array(&[0.0, theta, theta, 0.0])
+    }
+
+    pub fn vee(omega: Mat2) -> f32 {
+        omega.x_axis.y
+    }
+}
+
+impl Mul<Vec2> for SO2 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: Vec2) -> Self::Output {
+        Vec2::new(
+            self.z.x * rhs.x - self.z.y * rhs.y,
+            self.z.y * rhs.x + self.z.x * rhs.y,
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_identity() {
+        let identity = SO2::IDENTITY;
+        assert_eq!(identity.z, Vec2::new(1.0, 0.0));
+    }
+
+    #[test]
+    fn test_new() {
+        let z = Vec2::new(0.5, 0.5);
+        let so2 = SO2::new(z);
+        assert_eq!(so2.z, z);
+    }
+
+    #[test]
+    fn test_from_matrix() {
+        {
+            let mat = Mat2::from_cols_array(&Mat2::IDENTITY.to_cols_array());
+            let so2 = SO2::from_matrix(mat);
+            assert_eq!(so2.z, Vec2::new(1.0, 0.0));
+        }
+        {
+            let mat = Mat2::from_cols_array(&[0.6, -0.8, 0.8, 0.6]);
+            let so2 = SO2::from_matrix(mat);
+            assert_eq!(so2.z, Vec2::new(0.6, 0.8));
+        }
+    }
+
+    #[test]
+    fn test_as_matrix() {
+        let so2 = SO2::new(Vec2::new(0.6, 0.8));
+        let expected = Mat2::from_cols_array(&[0.6, -0.8, 0.8, 0.6]);
+        assert_eq!(so2.matrix(), expected);
+    }
+
+    #[test]
+    fn test_inverse() {
+        {
+            let so2 = SO2::IDENTITY;
+            let inv = so2.inverse();
+            let expected = Vec2::new(1.0, 0.0);
+            assert!((inv.z - expected).length() < 1e-10);
+        }
+        {
+            let so2 = SO2::new(Vec2::new(0.6, 0.8));
+            let inv = so2.inverse();
+            let expected = Vec2::new(0.6, -0.8);
+            assert!((inv.z - expected).length() < 1e-10);
+        }
+    }
+
+    #[test]
+    fn test_adjoint() {
+        let so2 = SO2::IDENTITY;
+        assert_eq!(so2.adjoint(), so2.matrix());
+    }
+
+    #[test]
+    fn test_exp() {
+        let theta = std::f32::consts::PI / 4.0;
+        let so2 = SO2::exp(theta);
+        assert!((so2.z.x - theta.cos()).abs() < 1e-10);
+        assert!((so2.z.y - theta.sin()).abs() < 1e-10);
+    }
+
+    #[test]
+    fn test_log() {
+        let so2 = SO2::new(Vec2::new(0.6, 0.8));
+        let theta = so2.log();
+        assert!((theta - 0.9273).abs() < 1e-4);
+    }
+
+    #[test]
+    fn test_hat() {
+        let theta = 0.5;
+        let expected = Mat2::from_cols_array(&[0.0, 0.5, 0.5, 0.0]);
+        assert_eq!(SO2::hat(theta), expected);
+    }
+
+    #[test]
+    fn test_vee() {
+        let omega = Mat2::from_cols_array(&[0.0, 0.5, 0.5, 0.0]);
+        let theta = SO2::vee(omega);
+        assert_eq!(theta, 0.5);
+    }
+}
---
+++ crates/kornia-lie/src/so3.rs
@@ -0,0 +1,238 @@
+use glam::{Affine3A, Mat3A, Mat4, Quat, Vec3A};
+use rand::Rng;
+use std::fmt::Debug;
+use std::ops::Mul;
+
+#[derive(Debug, Clone, Copy)]
+pub struct SO3 {
+    pub q: Quat,
+}
+
+impl SO3 {
+    pub const IDENTITY: Self = Self { q: Quat::IDENTITY };
+
+    pub fn from_quaternion(quat: Quat) -> Self {
+        Self { q: quat }
+    }
+
+    pub fn from_matrix(mat: &Mat3A) -> Self {
+        Self {
+            q: Quat::from_mat3a(mat),
+        }
+    }
+
+    pub fn from_matrix4(mat: &Mat4) -> Self {
+        Self {
+            q: Quat::from_mat4(mat),
+        }
+    }
+
+    pub fn from_random() -> Self {
+        let mut rng = rand::rng();
+
+        let r1: f32 = rng.random();
+        let r2: f32 = rng.random();
+        let r3: f32 = rng.random();
+
+        let one_minus_r1_sqrt = (1.0 - r1).sqrt();
+        let r1_sqrt = r1.sqrt();
+        let w = one_minus_r1_sqrt.sqrt() * (2.0 * std::f32::consts::PI * r2).sin();
+        let x = one_minus_r1_sqrt.sqrt() * (2.0 * std::f32::consts::PI * r2).cos();
+        let y = r1_sqrt * (2.0 * std::f32::consts::PI * r3).sin();
+        let z = r1_sqrt * (2.0 * std::f32::consts::PI * r3).cos();
+
+        Self {
+            q: Quat::from_xyzw(x, y, z, w),
+        }
+    }
+
+    pub fn matrix(&self) -> Mat3A {
+        Affine3A::from_quat(self.q).matrix3
+    }
+
+    pub fn adjoint(&self) -> Mat3A {
+        self.matrix()
+    }
+
+    pub fn inverse(&self) -> Self {
+        Self {
+            q: self.q.inverse(),
+        }
+    }
+
+    /// Lie algebra -> Lie group
+    pub fn exp(v: Vec3A) -> Self {
+        let theta = v.dot(v).sqrt();
+        let theta_half = theta / 2.0;
+
+        let (w, b) = if theta != 0.0 {
+            (theta_half.cos(), theta_half.sin() / theta)
+        } else {
+            (1.0, 0.0)
+        };
+        let xyz = b * v;
+
+        Self {
+            q: Quat::from_xyzw(xyz.x, xyz.y, xyz.z, w),
+        }
+    }
+
+    /// Lie group -> Lie algebra
+    pub fn log(&self) -> Vec3A {
+        let real = self.q.w;
+        let vec = Vec3A::new(self.q.x, self.q.y, self.q.z);
+
+        let theta = vec.dot(vec).sqrt();
+        let omega = if theta != 0.0 {
+            vec * 2.0 * real.acos() / theta
+        } else {
+            vec * 2.0 / real
+        };
+
+        omega
+    }
+
+    /// Vector space -> Lie algebra
+    pub fn hat(v: Vec3A) -> Mat3A {
+        Mat3A::from_cols_array(&[0.0, -v.z, v.y, v.z, 0.0, -v.x, -v.y, v.x, 0.0])
+    }
+
+    /// Lie algebra -> vector space
+    pub fn vee(omega: Mat3A) -> Vec3A {
+        Vec3A::from_array([omega.z_axis.y, omega.x_axis.z, omega.y_axis.x])
+    }
+    pub fn vee4(omega: Mat4) -> Vec3A {
+        Vec3A::from_array([omega.z_axis.y, omega.x_axis.z, omega.y_axis.x])
+    }
+
+    pub fn left_jacobian(v: Vec3A) -> Mat3A {
+        let skew = Self::hat(v);
+        let theta = v.dot(v).sqrt();
+        let ident = Mat3A::IDENTITY;
+
+        ident
+            + ((1.0 - theta.cos()) / theta.powi(2)) * skew
+            + ((theta - theta.sin()) / theta.powi(3)) * (skew * skew)
+    }
+
+    pub fn right_jacobian(v: Vec3A) -> Mat3A {
+        let skew = Self::hat(v);
+        let theta = v.dot(v).sqrt();
+        let ident = Mat3A::IDENTITY;
+
+        ident - ((1.0 - theta.cos()) / theta.powi(2)) * skew
+            + ((theta - theta.sin()) / theta.powi(3)) * (skew * skew)
+    }
+}
+
+impl Mul for SO3 {
+    type Output = SO3;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_identity() {
+        let s = SO3::IDENTITY;
+        assert_eq!(s.q, Quat::from_xyzw(0.0, 0.0, 0.0, 1.0));
+    }
+
+    #[test]
+    fn test_from_quaternion() {
+        let q = Quat::from_xyzw(4.0, -2.0, 1.0, 3.5);
+        let s = SO3::from_quaternion(q);
+        assert_eq!(s.q, q);
+    }
+
+    #[test]
+    fn test_from_matrix() {
+        let mat = Mat3A::from_cols_array(&[1.0, 0.0, 0.0, 0.0, 0.6, 0.8, 0.0, -0.8, 0.6]);
+        let s = SO3::from_matrix(&mat);
+        assert!((s.q - Quat::from_xyzw(0.5, 0.0, 0.0, 1.0).normalize()).length() < 1e-5);
+    }
+
+    #[test]
+    fn test_log() {
+        {
+            let so3 = SO3::from_quaternion(Quat::from_xyzw(1.0, 1.0, 1.0, 1.0));
+            let log = so3.log();
+            assert!((log - Vec3A::new(0.0, 0.0, 0.0)).length() < 1e-5);
+        }
+
+        {
+            let so3 = SO3::exp(Vec3A::new(1.0, 0.0, 0.0));
+            let log = so3.log();
+            assert!((log - Vec3A::new(1.0, 0.0, 0.0)).length() < 1e-5);
+        }
+    }
+
+    #[test]
+    fn test_exp() {
+        let v = Vec3A::from_array([0.0, 0.0, 0.0]);
+        let s = SO3::exp(v);
+        assert_eq!(s.q, Quat::from_xyzw(0.0, 0.0, 0.0, 1.0));
+    }
+
+    #[test]
+    fn test_hat() {
+        let v = Vec3A::new(1.0, 2.0, 3.0);
+        let hat_v = SO3::hat(v);
+        assert_eq!(hat_v.x_axis.y, -3.0);
+        assert_eq!(hat_v.x_axis.z, 2.0);
+        assert_eq!(hat_v.y_axis.x, 3.0);
+        assert_eq!(hat_v.y_axis.z, -1.0);
+        assert_eq!(hat_v.z_axis.x, -2.0);
+        assert_eq!(hat_v.z_axis.y, 1.0);
+    }
+
+    #[test]
+    fn test_vee() {
+        {
+            let omega = SO3::hat(Vec3A::new(1.0, 2.0, 3.0));
+            let v = SO3::vee(omega);
+            assert!((v - Vec3A::new(1.0, 2.0, 3.0)).length() < 1e-5);
+        }
+    }
+
+    #[test]
+    fn test_adjoint() {
+        let so3 = SO3::exp(Vec3A::new(0.1, 0.2, 0.3));
+        let adjoint = so3.adjoint();
+        assert!(adjoint.is_finite());
+    }
+
+    #[test]
+    fn test_inverse() {
+        let so3 = SO3::exp(Vec3A::new(0.5, -0.2, 0.1));
+        let inv = so3.inverse();
+        let identity = so3.matrix() * inv.matrix();
+
+        let max_diff = (identity - Mat3A::IDENTITY)
+            .to_cols_array()
+            .iter()
+            .map(|&x| x.abs())
+            .fold(0.0, f32::max);
+
+        assert!(max_diff < 1e-5);
+    }
+
+    #[test]
+    fn test_left_jacobian() {
+        let v = Vec3A::new(0.1, 0.2, 0.3);
+        let left_jacobian = SO3::left_jacobian(v);
+        assert!(left_jacobian.is_finite());
+    }
+
+    #[test]
+    fn test_right_jacobian() {
+        let v = Vec3A::new(-0.1, 0.3, 0.2);
+        let right_jacobian = SO3::right_jacobian(v);
+        assert!(right_jacobian.is_finite());
+    }
+}
