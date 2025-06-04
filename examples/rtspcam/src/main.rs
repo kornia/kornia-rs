f10a57280ad4603a64a9a9c1068011517ae14d01
edgar <edgar.riba@gmail.com>
2025-06-04 17:31:08 +0200

    handle wrap

--- examples/rtspcam/src/main.rs
+++ examples/rtspcam/src/main.rs
@@ -90,10 +90,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // initialize images lazily
         let img_f32_ref = img_f32.get_or_insert_with(|| {
-            Image::<f32, 3, _>::from_size_val(img.size(), 0.0, CpuAllocator).unwrap()
+            Image::<f32, 3, _>::from_size_val(img.size(), 0.0, CpuAllocator)
+                .expect("Failed to create image")
         });
         let gray_ref = gray.get_or_insert_with(|| {
-            Image::<f32, 1, _>::from_size_val(img.size(), 0.0, CpuAllocator).unwrap()
+            Image::<f32, 1, _>::from_size_val(img.size(), 0.0, CpuAllocator)
+                .expect("Failed to create image")
         });
 
         // cast the image to floating point and convert to grayscale
