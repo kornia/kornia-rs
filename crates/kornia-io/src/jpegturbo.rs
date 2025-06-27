5adc7e80057063d7e26a1fa4ec5481257a52ee27
Edgar Riba <edgar.riba@gmail.com>
2025-06-27 15:13:13 +0200

    separate internal conv bit functions (#416)
    * separate internal conv bit functions
    
    * fix ioerror import

--- crates/kornia-io/src/jpegturbo.rs
+++ crates/kornia-io/src/jpegturbo.rs
@@ -1,4 +1,4 @@
-use crate::IoError;
+use crate::error::IoError;
 use kornia_image::{
     allocator::{CpuAllocator, ImageAllocator},
     Image, ImageError, ImageSize,
@@ -290,7 +290,7 @@ pub fn write_image_jpegturbo_rgb8<A: ImageAllocator>(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::IoError;
+    use crate::error::IoError;
 
     #[test]
     fn image_decoder() -> Result<(), JpegTurboError> {
