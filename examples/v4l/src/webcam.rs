a380b62401f9b9230b9eb9f9002d3f1b46ccfa6b
m0sys <21181457+m0sys@users.noreply.github.com>
2025-07-13 10:32:55 -0400

    chore: rename webcam example to v4l

--- examples/v4l/src/webcam.rs
+++ examples/v4l/src/webcam.rs
@@ -34,7 +34,7 @@ struct Args {
     debug: bool,
 }
 
-pub fn webcam_demo() -> Result<(), Box<dyn std::error::Error>> {
+pub fn v4l_demo() -> Result<(), Box<dyn std::error::Error>> {
     let args: Args = argh::from_env();
 
     // Create the recording stream
