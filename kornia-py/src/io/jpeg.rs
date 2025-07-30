2c4c984f30deea0848d69e22880594dbaaf3d5a6
edgar <edgar.riba@gmail.com>
2025-07-30 11:52:03 +0200

    io: expose decode jpeg image info

--- kornia-py/src/io/jpeg.rs
+++ kornia-py/src/io/jpeg.rs
@@ -1,5 +1,5 @@
-use crate::image::{FromPyImage, PyImage, ToPyImage};
-use kornia_image::{allocator::CpuAllocator, Image, ImageSize};
+use crate::image::{FromPyImage, PyImage, PyImageSize, ToPyImage};
+use kornia_image::{allocator::CpuAllocator, Image};
 use kornia_io::jpeg as J;
 use pyo3::prelude::*;
 
@@ -65,24 +65,17 @@ pub fn write_image_jpeg(file_path: &str, image: PyImage, mode: &str, quality: u8
 #[pyfunction]
 /// Decodes the JPEG Image from raw bytes.
 ///
-/// The following modes are supported:
-/// 1. "rgb" -> 8-bit RGB
-/// 2. "mono" -> 8-bit Monochrome
-///
 /// ```py
 /// import kornia_rs as K
 ///
-/// img = K.decode_image_jpeg(bytes(img_data), (32, 32), "rgb")
+/// img = K.decode_image_jpeg(bytes(img_data))
 /// ```
-///
-pub fn decode_image_jpeg(src: &[u8], image_shape: (usize, usize), mode: &str) -> PyResult<PyImage> {
-    let image_shape = ImageSize {
-        width: image_shape.1,
-        height: image_shape.0,
-    };
+pub fn decode_image_jpeg(src: &[u8]) -> PyResult<PyImage> {
+    let (image_shape, num_channels) = J::decode_image_jpeg_info(src)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
 
-    let result = match mode {
-        "rgb" => {
+    let result = match num_channels {
+        3 => {
             let mut output_image =
                 Image::<u8, 3, _>::from_size_val(image_shape, 0, CpuAllocator)
                     .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
@@ -90,7 +83,7 @@ pub fn decode_image_jpeg(src: &[u8], image_shape: (usize, usize), mode: &str) ->
                 .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
             output_image.to_pyimage()
         }
-        "mono" => {
+        1 => {
             let mut output_image =
                 Image::<u8, 1, _>::from_size_val(image_shape, 0, CpuAllocator)
                     .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
@@ -98,18 +91,26 @@ pub fn decode_image_jpeg(src: &[u8], image_shape: (usize, usize), mode: &str) ->
                 .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
             output_image.to_pyimage()
         }
-        _ => {
+        ch => {
             return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
-                String::from(
-                    r#"\
-        The following are the supported values of mode:
-        1) "rgb" -> 8-bit RGB
-        2) "mono" -> 8-bit Monochrome
-        "#,
-                ),
+                String::from(format!("Unsupported number of channels: {}", ch)),
             ))
         }
     };
 
     Ok(result)
+}
+
+#[pyfunction]
+/// Decodes the JPEG Image from raw bytes.
+///
+/// ```py
+/// import kornia_rs as K
+///
+/// (size, num_channels) = K.decode_image_jpeg_info(bytes(img_data))
+/// ```
+pub fn decode_image_jpeg_info(src: &[u8]) -> PyResult<(PyImageSize, u8)> {
+    let (image_shape, num_channels) = J::decode_image_jpeg_info(src)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
+    Ok((image_shape.into(), num_channels))
 }
