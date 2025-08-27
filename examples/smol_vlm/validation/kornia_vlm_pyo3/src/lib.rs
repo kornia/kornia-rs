d8fb3b2a2fa0e323aed906910ff13558c5be9969
Andrew Shen <andrewshen31@gmail.com>
2025-08-27 13:13:49 -0700

    improved evaluation performance

--- examples/smol_vlm/validation/kornia_vlm_pyo3/src/lib.rs
+++ examples/smol_vlm/validation/kornia_vlm_pyo3/src/lib.rs
@@ -2,6 +2,59 @@ use kornia_io::jpeg::read_image_jpeg_rgb8;
 use kornia_vlm::smolvlm::{utils::SmolVlmConfig, SmolVlm};
 use pyo3::prelude::*;
 
+#[pyclass]
+pub struct SmolVLMInterface {
+    model: SmolVlm,
+}
+
+#[pymethods]
+impl SmolVLMInterface {
+    #[new]
+    fn new() -> Self {
+        SmolVLMInterface {
+            model: SmolVlm::new(SmolVlmConfig {
+                do_sample: false, // set to false for greedy decoding
+                seed: 420,
+                ..Default::default()
+            })
+            .unwrap(),
+        }
+    }
+
+    #[pyo3(signature = (text_prompt, sample_length, image_paths=vec![]))]
+    fn generate_raw(
+        &mut self,
+        text_prompt: String,
+        sample_length: usize,
+        image_paths: Vec<String>,
+    ) -> PyResult<String> {
+        // NOTE: e is inferred and there's two types of error to handle (SmolVLM & kornia io)
+        let map_error = |e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("{:?}", e));
+        let map_krn_error =
+            |e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("{:?}", e));
+
+        // read the image
+        let images = image_paths
+            .into_iter()
+            .map(|p| read_image_jpeg_rgb8(&p))
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(map_krn_error)?;
+
+        // generate a caption of the image
+        let caption = self
+            .model
+            .inference_raw(&text_prompt, images, sample_length)
+            .map_err(map_error)?;
+
+        Ok(caption)
+    }
+
+    fn clear_context(&mut self) -> PyResult<String> {
+        self.model.clear_context();
+        Ok("Context cleared".to_string())
+    }
+}
+
 #[pyfunction]
 #[pyo3(signature = (text_prompt, sample_length, image_paths=vec![]))]
 fn generate(
@@ -71,5 +124,6 @@ fn generate_raw(
 fn kornia_vlm_pyo3(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(generate, m)?)?;
     m.add_function(wrap_pyfunction!(generate_raw, m)?)?;
+    m.add_class::<SmolVLMInterface>()?;
     Ok(())
 }
