165a6e9c6fe2f93127eee6a2ea9cc96df4d709c6
Andrew Shen <andrewshen31@gmail.com>
2025-09-05 04:28:49 +0000

    invoke CI/CD pipeline

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+
 apt-get update && apt-get install -y cmake nasm libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev # Install NASM and GStreamer
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/bin # Install Just for command runner
 curl -LsSf https://astral.sh/uv/install.sh | sh # Install UV for manage python virtual environments
