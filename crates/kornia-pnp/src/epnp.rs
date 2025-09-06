853a11fd94825b0cb87ff528120dfaa69ef4fa65
cjpurackal <christie.purackal@audioscenic.com>
2025-09-06 13:14:44 +0100

    fix: remove redundancy for projection ops

--- crates/kornia-pnp/src/epnp.rs
+++ crates/kornia-pnp/src/epnp.rs
@@ -2,7 +2,9 @@
 //! Paper: [Lepetit et al., IJCV 2009](https://www.tugraz.at/fileadmin/user_upload/Institute/ICG/Images/team_lepetit/publications/lepetit_ijcv08.pdf)
 //! Reference: [OpenCV EPnP implementation](https://github.com/opencv/opencv/blob/4.x/modules/calib3d/src/epnp.cpp)
 
-use crate::ops::{compute_centroid, gauss_newton};
+use crate::ops::{
+    compute_centroid, gauss_newton, intrinsics_as_vectors, pose_to_rt, project_sq_error,
+};
 use crate::pnp::{NumericTol, PnPError, PnPResult, PnPSolver};
 use glam::{Mat3, Mat3A, Vec3};
 use kornia_imgproc::calibration::{
@@ -228,22 +230,8 @@ fn rmse_px(
         });
     }
 
-    let fx = k[0][0];
-    let fy = k[1][1];
-    let cx = k[0][2];
-    let cy = k[1][2];
-
-    // Pre-compute matrix and vectors in glam form for SIMD-friendly ops
-    let r_mat = Mat3::from_cols(
-        Vec3::new(r[0][0], r[1][0], r[2][0]),
-        Vec3::new(r[0][1], r[1][1], r[2][1]),
-        Vec3::new(r[0][2], r[1][2], r[2][2]),
-    );
-    let t_vec = Vec3::new(t[0], t[1], t[2]);
-
-    // Pack intrinsics so that (intr_x ⋅ Pc) / z = fx * x / z + cx
-    let intr_x = Vec3::new(fx, 0.0, cx);
-    let intr_y = Vec3::new(0.0, fy, cy);
+    let (r_mat, t_vec) = pose_to_rt(r, t);
+    let (intr_x, intr_y) = intrinsics_as_vectors(k);
 
     let mut sum_sq = 0.0;
     let n = points_world.len() as f32;
