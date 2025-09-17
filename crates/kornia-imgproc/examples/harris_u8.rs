d57e975028a7b658795825cd62f16f3dbd71241d
5usu <srdwivedi_b23@tx.vjti.ac.in>
2025-09-17 12:54:24 +0530

    Revert "deleted examples"
    This reverts commit 0daf9ea341bbd21475cd3c370ff0aee501166357.

---
+++ crates/kornia-imgproc/examples/harris_u8.rs
@@ -0,0 +1,38 @@
+use kornia_image::Image;
+use kornia_imgproc::features::HarrisResponse;
+use kornia_tensor::CpuAllocator;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a simple 9x9 synthetic u8 image with a bright rectangle
+    let src = Image::from_size_slice(
+        [9, 9].into(),
+        &[
+            0u8, 0, 0, 0, 0, 0, 0, 0, 0,
+            0u8, 0, 0, 0, 0, 0, 0, 0, 0,
+            0u8, 0, 255, 255, 255, 255, 255, 0, 0,
+            0u8, 0, 255, 255, 255, 255, 255, 0, 0,
+            0u8, 0, 255, 255, 255, 255, 255, 0, 0,
+            0u8, 0, 255, 255, 255, 255, 255, 0, 0,
+            0u8, 0, 255, 255, 255, 255, 255, 0, 0,
+            0u8, 0, 0, 0, 0, 0, 0, 0, 0,
+            0u8, 0, 0, 0, 0, 0, 0, 0, 0,
+        ],
+        CpuAllocator,
+    )?;
+
+    let mut dst = Image::<f32, 1, _>::from_size_val(src.size(), 0.0, CpuAllocator)?;
+    HarrisResponse::new(src.size()).compute_u8(&src, &mut dst)?;
+
+    // Print center 5x5 block of responses for quick inspection
+    for r in 2..7 {
+        for c in 2..7 {
+            let v = dst.as_slice()[r * dst.cols() + c];
+            print!("{v:.6} ");
+        }
+        println!();
+    }
+
+    Ok(())
+}
+
+
