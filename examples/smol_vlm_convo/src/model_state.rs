e7cca4949969801342469216733d248b80ee90dc
Andrew Shen <andrewshen31@gmail.com>
2025-09-19 07:04:04 +0000

    proper thread drop handling

--- examples/smol_vlm_convo/src/model_state.rs
+++ examples/smol_vlm_convo/src/model_state.rs
@@ -1,6 +1,6 @@
 use kornia_vlm::smolvlm::{utils::SmolVlmConfig, SmolVlm};
 use std::sync::mpsc;
-use std::thread;
+use std::thread::{self, JoinHandle};
 
 use kornia_image::Image;
 use kornia_tensor::CpuAllocator;
@@ -25,12 +25,13 @@ pub enum ModelResponse {
 
 pub struct ModelStateHandle {
     pub tx: mpsc::Sender<ModelRequest>,
+    task_handle: Option<JoinHandle<()>>,
 }
 
 impl ModelStateHandle {
     pub fn new() -> Self {
         let (tx, rx) = mpsc::channel::<ModelRequest>();
-        thread::spawn(move || {
+        let task_handle = thread::spawn(move || {
             let mut config = SmolVlmConfig {
                 temp: 1.0,
                 ..Default::default()
@@ -87,6 +88,22 @@ impl ModelStateHandle {
                 }
             }
         });
-        Self { tx }
+        Self { 
+            tx,
+            task_handle: Some(task_handle),
+        }
+    }
+}
+
+impl Drop for ModelStateHandle {
+    fn drop(&mut self) {
+        // Signal the thread to stop by dropping the sender
+        // This will cause the receiver to return an error and break the loop
+        drop(self.tx.clone());
+        
+        // Wait for the thread to finish
+        if let Some(handle) = self.task_handle.take() {
+            let _ = handle.join();
+        }
     }
 }
