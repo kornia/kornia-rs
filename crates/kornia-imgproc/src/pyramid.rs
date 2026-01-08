42df02c937ff7febc324d8dd2643e96fa830e413
Incharajayaram <incharajayaram2020@gmail.com>
2026-01-08 21:20:20 +0530

    docs(imgproc): fix pyrdown doctest to include CpuAllocator and concise sample data

--- crates/kornia-imgproc/src/pyramid.rs
+++ crates/kornia-imgproc/src/pyramid.rs
@@ -111,25 +111,25 @@ pub fn pyrup<const C: usize, A1: ImageAllocator, A2: ImageAllocator>(
 ///
 /// ```
 /// use kornia_image::{Image, ImageSize};
+/// use kornia_image::allocator::CpuAllocator;
 /// use kornia_imgproc::pyramid::pyrdown;
 ///
-/// let image = Image::<f32, 3>::new(
+/// let image = Image::<f32, 3, _>::new(
 ///     ImageSize {
 ///         width: 4,
 ///         height: 4,
 ///     },
-///     vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0,
-///          12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0,
-///          24.0, 25.0, 26.0, 27.0, 28.0, 29.0, 30.0, 31.0, 32.0, 33.0, 34.0, 35.0,
-///          36.0, 37.0, 38.0, 39.0, 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0],
+///     (0..48).map(|x| x as f32).collect(),
+///     CpuAllocator,
 /// ).unwrap();
 ///
-/// let mut downsampled = Image::<f32, 3>::from_size_val(
+/// let mut downsampled = Image::<f32, 3, _>::from_size_val(
 ///     ImageSize {
 ///         width: 2,
 ///         height: 2,
 ///     },
 ///     0.0,
+///     CpuAllocator,
 /// ).unwrap();
 ///
 /// pyrdown(&image, &mut downsampled).unwrap();
