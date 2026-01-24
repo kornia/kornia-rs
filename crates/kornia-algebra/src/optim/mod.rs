e1e7b92d13c452d546e8119417f7d8e619219d02
cjpurackal <christie.purackal@audioscenic.com>
2026-01-24 16:09:38 +0000

    feat: add dynamic matrix,vector via nalgebra

--- crates/kornia-algebra/src/optim/mod.rs
+++ crates/kornia-algebra/src/optim/mod.rs
@@ -1,8 +1,12 @@
 mod factor;
 mod variable;
 mod problem;
+mod levenberg_marquardt;
 
 // Re-exports
 pub use factor::{Factor, FactorError, FactorResult, LinearizationResult, PriorFactor};
 pub use variable::{Variable, VariableType};
 pub use problem::{Problem, ProblemError};
+pub use levenberg_marquardt::{
+    LevenbergMarquardt, OptimizerError, OptimizerResult, TerminationReason,
+};
